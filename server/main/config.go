d41c747a7a6f0394089432a9a8e474207fd450bd
Jesús Espino <jespinog@gmail.com>
2020-10-09 10:27:20 +0200

    Simplifying config using viper

--- server/main/config.go
+++ server/main/config.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"os"
+
+	"github.com/spf13/viper"
 )
 
 // Configuration is the app configuration stored in a json file
@@ -17,49 +17,27 @@ type Configuration struct {
 	FilesPath      string `json:"filespath"`
 }
 
-func readConfigFile() Configuration {
-	fileName := "config.json"
-	if !fileExists(fileName) {
-		log.Println(`config.json not found, using default settings`)
-		return Configuration{}
+func readConfigFile() (*Configuration, error) {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("json")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	viper.SetDefault("ServerRoot", "http://localhost")
+	viper.SetDefault("Port", 8000)
+	viper.SetDefault("DBType", "sqlite3")
+	viper.SetDefault("DBConfigString", "./octo.db")
+	viper.SetDefault("WebPath", "./pack")
+	viper.SetDefault("FilesPath", "./files")
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		return nil, err
 	}
 
-	file, _ := os.Open(fileName)
-	defer file.Close()
-	decoder := json.NewDecoder(file)
 	configuration := Configuration{}
-	err := decoder.Decode(&configuration)
-	if err != nil {
-		log.Fatal("Invalid config.json", err)
-	}
-
-	// Apply defaults
-	if len(configuration.ServerRoot) < 1 {
-		configuration.ServerRoot = "http://localhost"
-	}
-
-	if configuration.Port == 0 {
-		configuration.Port = 8000
-	}
-
-	if len(configuration.DBType) < 1 {
-		configuration.DBType = "sqlite3"
-	}
-
-	if len(configuration.DBConfigString) < 1 {
-		configuration.DBConfigString = "./octo.db"
-	}
-
-	if len(configuration.WebPath) < 1 {
-		configuration.WebPath = "./pack"
-	}
-
-	if len(configuration.FilesPath) < 1 {
-		configuration.FilesPath = "./files"
-	}
+	viper.Unmarshal(&configuration)
 
 	log.Println("readConfigFile")
 	log.Printf("%+v", configuration)
 
-	return configuration
+	return &configuration, nil
 }
