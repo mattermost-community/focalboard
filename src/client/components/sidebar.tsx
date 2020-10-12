e29d2ef9bafe9b8b96b2dbe292fe66c491e86af0
Chen-I Lim <chenilim@gmail.com>
2020-10-12 11:40:09 -0700

    Sidebar style

--- src/client/components/sidebar.tsx
+++ src/client/components/sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Board } from "../board"
 import { WorkspaceTree } from "../workspaceTree"
 
 type Props = {
@@ -14,14 +15,19 @@ class Sidebar extends React.Component<Props> {
 		return (
 			<div className="octo-sidebar">
 				{ boards.map(board => {
+					const displayTitle = board.title || "(Untitled Board)"
 					return (
-						<div>
-							{board.title || "(Untitled Board)"}
+						<div className="octo-sidebar-item" onClick={() => { this.boardClicked(board) }}>
+							{board.icon ? `${board.icon} ${displayTitle}` : displayTitle}
 						</div>
 					)
 				})}
 			</div>
 		)
+	}
+
+	private boardClicked(board: Board) {
+		// TODO
 	}
 }
 
