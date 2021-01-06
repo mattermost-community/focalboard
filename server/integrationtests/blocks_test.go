593e077fb961ae43cee8c797ae5f50e24acd64a8
Chen-I Lim <chenilim@gmail.com>
2021-01-05 19:47:18 -0800

    Fix Go integration tests

--- server/integrationtests/blocks_test.go
+++ server/integrationtests/blocks_test.go
@@ -13,28 +13,34 @@ func TestGetBlocks(t *testing.T) {
 	th := SetupTestHelper().InitBasic()
 	defer th.TearDown()
 
+	blocks, resp := th.Client.GetBlocks()
+	require.NoError(t, resp.Error)
+	initialCount := len(blocks)
+
 	blockID1 := utils.CreateGUID()
 	blockID2 := utils.CreateGUID()
 	newBlocks := []model.Block{
 		{
 			ID:       blockID1,
+			RootID:   blockID1,
 			CreateAt: 1,
 			UpdateAt: 1,
 			Type:     "board",
 		},
 		{
 			ID:       blockID2,
+			RootID:   blockID2,
 			CreateAt: 1,
 			UpdateAt: 1,
 			Type:     "board",
 		},
 	}
-	_, resp := th.Client.InsertBlocks(newBlocks)
+	_, resp = th.Client.InsertBlocks(newBlocks)
 	require.NoError(t, resp.Error)
 
-	blocks, resp := th.Client.GetBlocks()
+	blocks, resp = th.Client.GetBlocks()
 	require.NoError(t, resp.Error)
-	require.Len(t, blocks, 2)
+	require.Len(t, blocks, initialCount+2)
 
 	blockIDs := make([]string, len(blocks))
 	for i, b := range blocks {
@@ -48,6 +54,10 @@ func TestPostBlock(t *testing.T) {
 	th := SetupTestHelper().InitBasic()
 	defer th.TearDown()
 
+	blocks, resp := th.Client.GetBlocks()
+	require.NoError(t, resp.Error)
+	initialCount := len(blocks)
+
 	blockID1 := utils.CreateGUID()
 	blockID2 := utils.CreateGUID()
 	blockID3 := utils.CreateGUID()
@@ -55,6 +65,7 @@ func TestPostBlock(t *testing.T) {
 	t.Run("Create a single block", func(t *testing.T) {
 		block := model.Block{
 			ID:       blockID1,
+			RootID:   blockID1,
 			CreateAt: 1,
 			UpdateAt: 1,
 			Type:     "board",
@@ -66,20 +77,27 @@ func TestPostBlock(t *testing.T) {
 
 		blocks, resp := th.Client.GetBlocks()
 		require.NoError(t, resp.Error)
-		require.Len(t, blocks, 1)
-		require.Equal(t, blockID1, blocks[0].ID)
+		require.Len(t, blocks, initialCount+1)
+
+		blockIDs := make([]string, len(blocks))
+		for i, b := range blocks {
+			blockIDs[i] = b.ID
+		}
+		require.Contains(t, blockIDs, blockID1)
 	})
 
 	t.Run("Create a couple of blocks in the same call", func(t *testing.T) {
 		newBlocks := []model.Block{
 			{
 				ID:       blockID2,
+				RootID:   blockID2,
 				CreateAt: 1,
 				UpdateAt: 1,
 				Type:     "board",
 			},
 			{
 				ID:       blockID3,
+				RootID:   blockID3,
 				CreateAt: 1,
 				UpdateAt: 1,
 				Type:     "board",
@@ -91,7 +109,7 @@ func TestPostBlock(t *testing.T) {
 
 		blocks, resp := th.Client.GetBlocks()
 		require.NoError(t, resp.Error)
-		require.Len(t, blocks, 3)
+		require.Len(t, blocks, initialCount+3)
 
 		blockIDs := make([]string, len(blocks))
 		for i, b := range blocks {
@@ -105,6 +123,7 @@ func TestPostBlock(t *testing.T) {
 	t.Run("Update a block", func(t *testing.T) {
 		block := model.Block{
 			ID:       blockID1,
+			RootID:   blockID1,
 			CreateAt: 1,
 			UpdateAt: 20,
 			Type:     "board",
@@ -116,7 +135,7 @@ func TestPostBlock(t *testing.T) {
 
 		blocks, resp := th.Client.GetBlocks()
 		require.NoError(t, resp.Error)
-		require.Len(t, blocks, 3)
+		require.Len(t, blocks, initialCount+3)
 
 		var updatedBlock model.Block
 		for _, b := range blocks {
@@ -133,10 +152,15 @@ func TestDeleteBlock(t *testing.T) {
 	th := SetupTestHelper().InitBasic()
 	defer th.TearDown()
 
+	blocks, resp := th.Client.GetBlocks()
+	require.NoError(t, resp.Error)
+	initialCount := len(blocks)
+
 	blockID := utils.CreateGUID()
 	t.Run("Create a block", func(t *testing.T) {
 		block := model.Block{
 			ID:       blockID,
+			RootID:   blockID,
 			CreateAt: 1,
 			UpdateAt: 1,
 			Type:     "board",
@@ -148,8 +172,14 @@ func TestDeleteBlock(t *testing.T) {
 
 		blocks, resp := th.Client.GetBlocks()
 		require.NoError(t, resp.Error)
-		require.Len(t, blocks, 1)
-		require.Equal(t, blockID, blocks[0].ID)
+		require.Len(t, blocks, initialCount+1)
+
+		blockIDs := make([]string, len(blocks))
+		for i, b := range blocks {
+			blockIDs[i] = b.ID
+		}
+		require.Contains(t, blockIDs, blockID)
+
 	})
 
 	t.Run("Delete a block", func(t *testing.T) {
@@ -158,13 +188,17 @@ func TestDeleteBlock(t *testing.T) {
 
 		blocks, resp := th.Client.GetBlocks()
 		require.NoError(t, resp.Error)
-		require.Len(t, blocks, 0)
+		require.Len(t, blocks, initialCount)
 	})
 }
 
 func TestGetSubtree(t *testing.T) {
 	th := SetupTestHelper().InitBasic()
 	defer th.TearDown()
+
+	blocks, resp := th.Client.GetBlocks()
+	require.NoError(t, resp.Error)
+	initialCount := len(blocks)
 
 	parentBlockID := utils.CreateGUID()
 	childBlockID1 := utils.CreateGUID()
@@ -173,12 +207,14 @@ func TestGetSubtree(t *testing.T) {
 		newBlocks := []model.Block{
 			{
 				ID:       parentBlockID,
+				RootID:   parentBlockID,
 				CreateAt: 1,
 				UpdateAt: 1,
 				Type:     "board",
 			},
 			{
 				ID:       childBlockID1,
+				RootID:   parentBlockID,
 				ParentID: parentBlockID,
 				CreateAt: 2,
 				UpdateAt: 2,
@@ -186,6 +222,7 @@ func TestGetSubtree(t *testing.T) {
 			},
 			{
 				ID:       childBlockID2,
+				RootID:   parentBlockID,
 				ParentID: parentBlockID,
 				CreateAt: 2,
 				UpdateAt: 2,
@@ -198,8 +235,13 @@ func TestGetSubtree(t *testing.T) {
 
 		blocks, resp := th.Client.GetBlocks()
 		require.NoError(t, resp.Error)
-		require.Len(t, blocks, 1)
-		require.Equal(t, parentBlockID, blocks[0].ID)
+		require.Len(t, blocks, initialCount+1) // GetBlocks returns root blocks (null ParentID)
+
+		blockIDs := make([]string, len(blocks))
+		for i, b := range blocks {
+			blockIDs[i] = b.ID
+		}
+		require.Contains(t, blockIDs, parentBlockID)
 	})
 
 	t.Run("Get subtree for parent ID", func(t *testing.T) {
