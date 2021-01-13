dfb45021de2f4a1edc52aaf4ee1c4779f2910156
Chen-I Lim <chenilim@gmail.com>
2021-01-13 11:48:23 -0800

    Fix switch state. Show share state text

--- webapp/src/components/shareBoardComponent.tsx
+++ webapp/src/components/shareBoardComponent.tsx
@@ -8,6 +8,7 @@ import {ISharing} from '../blocks/sharing'
 import client from '../octoClient'
 
 import {Utils} from '../utils'
+import {sendFlashMessage} from '../components/flashMessages'
 
 import Button from '../widgets/buttons/button'
 import Switch from '../widgets/switch'
@@ -42,27 +43,34 @@ class ShareBoardComponent extends React.PureComponent<Props, State> {
         const {intl} = this.props
         const {sharing} = this.state
 
-        const isSharing = sharing && sharing.id === this.props.boardId && sharing.enabled
+        const isSharing = Boolean(sharing && sharing.id === this.props.boardId && sharing.enabled)
         const readToken = (sharing && isSharing) ? sharing.token : ''
         const shareUrl = new URL(window.location.toString())
         shareUrl.searchParams.set('r', readToken)
 
+        let stateDescription: string
+        if (isSharing) {
+            stateDescription = intl.formatMessage({id: 'ShareBoard.unshare', defaultMessage: 'Anyone with the link can view this board'})
+        } else {
+            stateDescription = intl.formatMessage({id: 'ShareBoard.share', defaultMessage: 'Publish to web and share this board to anyone'})
+        }
         return (
             <Modal
                 onClose={this.props.onClose}
             >
                 <div className='ShareBoardComponent'>
                     <div className='row'>
-                        <div>{intl.formatMessage({id: 'ShareBoard.share', defaultMessage: 'Publish to web and share this board to anyone'})}</div>
+                        <div>{stateDescription}</div>
                         <div className='spacer'/>
                         <Switch
                             isOn={Boolean(isSharing)}
                             onChanged={this.onShareChanged}
                         />
                     </div>
-                    {isSharing &&
+                    {isSharing && <>
                         <div className='row'>
                             <input
+                                key={shareUrl.toString()}
                                 className='shareUrl'
                                 readOnly={true}
                                 value={shareUrl.toString()}
@@ -77,7 +85,12 @@ class ShareBoardComponent extends React.PureComponent<Props, State> {
                                 {this.state.wasCopied ? intl.formatMessage({id: 'ShareBoard.copiedLink', defaultMessage: 'Copied!'}) : intl.formatMessage({id: 'ShareBoard.copyLink', defaultMessage: 'Copy link'})}
                             </Button>
                         </div>
-                    }
+                        <div className='row'>
+                            <Button onClick={this.onRegenerateToken}>
+                                {intl.formatMessage({id: 'ShareBoard.regenerateToken', defaultMessage: 'Regenerate token'})}
+                            </Button>
+                        </div>
+                    </>}
                 </div>
             </Modal>
         )
@@ -96,9 +109,23 @@ class ShareBoardComponent extends React.PureComponent<Props, State> {
         const sharing: ISharing = this.state.sharing || this.createSharingInfo()
         sharing.id = this.props.boardId
         sharing.enabled = isOn
-        sharing.token = Utils.createGuid()
         await client.setSharing(sharing)
         await this.loadData()
+    }
+
+    private onRegenerateToken = async () => {
+        const {intl} = this.props
+        // eslint-disable-next-line no-alert
+        const accept = window.confirm(intl.formatMessage({id: 'ShareBoard.confirmRegenerateToken', defaultMessage: 'This will invalidate previously shared links. Continue?'}))
+        if (accept) {
+            const sharing: ISharing = this.state.sharing || this.createSharingInfo()
+            sharing.token = Utils.createGuid()
+            await client.setSharing(sharing)
+            await this.loadData()
+
+            const description = intl.formatMessage({id: 'ShareBoard.tokenRegenrated', defaultMessage: 'Token regenerated'})
+            sendFlashMessage({content: description, severity: 'low'})
+        }
     }
 }
 
