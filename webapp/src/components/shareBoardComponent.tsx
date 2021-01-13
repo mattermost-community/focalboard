2b208026560ee7e6730e1dd507fc10faac6e0a55
Chen-I Lim <chenilim@gmail.com>
2021-01-13 10:37:17 -0800

    ShareBoard WIP

--- webapp/src/components/shareBoardComponent.tsx
+++ webapp/src/components/shareBoardComponent.tsx
@@ -3,22 +3,53 @@
 import React from 'react'
 import {injectIntl, IntlShape} from 'react-intl'
 
+import Button from '../widgets/buttons/button'
+import Switch from '../widgets/switch'
+
 import Modal from './modal'
+import './shareBoardComponent.scss'
 
 type Props = {
     onClose: () => void
     intl: IntlShape
 }
 
-class ShareBoardComponent extends React.PureComponent<Props> {
+type State = {
+    isShared?: boolean
+}
+
+class ShareBoardComponent extends React.PureComponent<Props, State> {
+    state: State = {}
+
     render(): JSX.Element {
+        const {intl} = this.props
+
         return (
             <Modal
                 onClose={this.props.onClose}
             >
-                {'TODO'}
+                <div className='ShareBoardComponent'>
+                    <div className='row'>
+                        <div>{intl.formatMessage({id: 'ShareBoard.share', defaultMessage: 'Publish to web and share this board to anyone'})}</div>
+                        <div className='spacer'/>
+                        <Switch
+                            isOn={Boolean(this.state.isShared)}
+                            onChanged={this.onShareChanged}
+                        />
+                    </div>
+                    {this.state.isShared &&
+                        <div className='row'>
+                            <Button>{'Copy link'}</Button>
+                        </div>
+                    }
+                </div>
             </Modal>
         )
+    }
+
+    private onShareChanged = (isOn: boolean) => {
+        // TODO
+        this.setState({isShared: isOn})
     }
 }
 
