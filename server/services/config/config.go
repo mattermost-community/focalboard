633aef7421fe0d1ee3f30de74c44bdcbc53a1bae
Chen-I Lim <chenilim@gmail.com>
2021-03-22 14:45:08 -0700

    cleanup config logging

--- server/services/config/config.go
+++ server/services/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"log"
-	"reflect"
 
 	"github.com/spf13/viper"
 )
@@ -70,16 +69,6 @@ func ReadConfigFile() (*Configuration, error) {
 	}
 
 	log.Println("readConfigFile")
-
-	v := reflect.ValueOf(configuration)
-
-	values := make([]interface{}, v.NumField())
-	for i := 0; i < v.NumField(); i++ {
-		values[i] = v.Field(i).Interface()
-	}
-
-	log.Println(values)
-
 	log.Printf("%+v", removeSecurityData(configuration))
 
 	return &configuration, nil
