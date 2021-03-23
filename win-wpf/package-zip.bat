8c0a8e4cd3a21a81f17bea40876112558057213d
Chen-I Lim <chenilim@gmail.com>
2021-03-23 13:49:23 -0700

    Windows zip package

--- win-wpf/package-zip.bat
+++ win-wpf/package-zip.bat
@@ -3,4 +3,16 @@
 if exist dist\focalboard-win.zip del /q dist\focalboard-win.zip
 if not exist dist mkdir dist
 
-powershell Compress-Archive -Path Focalboard\bin\x64\Release\* -DestinationPath dist\focalboard-win.zip
+if exist temp del /s /f /q temp
+rmdir /s /q temp
+if not exist temp mkdir temp
+xcopy /e /i /y Focalboard\bin\x64\Release temp
+copy ..\build\MIT-COMPILED-LICENSE.md temp
+copy ..\NOTICE.txt temp
+copy ..\webapp\NOTICE.txt temp\webapp-NOTICE.txt
+
+echo --- Contents of temp ---
+dir /s /b temp
+echo ---
+
+powershell Compress-Archive -Path temp\* -DestinationPath dist\focalboard-win.zip
