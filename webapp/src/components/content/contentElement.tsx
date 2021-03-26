09693148aaa156669c88c9acc8a46d101027ead4
Jesús Espino <jespinog@gmail.com>
2021-03-26 16:20:35 +0100

    Migrating modal to functional component

--- webapp/src/components/content/contentElement.tsx
+++ webapp/src/components/content/contentElement.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react'
 import {injectIntl, IntlShape} from 'react-intl'
 
 import {IContentBlock} from '../../blocks/contentBlock'
