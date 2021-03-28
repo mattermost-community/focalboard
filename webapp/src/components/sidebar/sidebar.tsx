7f2a1ce1090bae510a42fce4a3259a25b1a168d9
Jesús Espino <jespinog@gmail.com>
2021-03-28 09:22:51 +0200

    Migrating sidebar to functional component

--- webapp/src/components/sidebar/sidebar.tsx
+++ webapp/src/components/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {injectIntl, IntlShape} from 'react-intl'
 
 import {loadTheme} from '../../theme'
@@ -22,123 +22,95 @@ type Props = {
     workspaceTree: WorkspaceTree,
     activeBoardId?: string
     setLanguage: (lang: string) => void,
-    intl: IntlShape
 }
 
-type State = {
-    isHidden: boolean
-    whiteLogo: boolean
-}
+const Sidebar = React.memo((props: Props) => {
+    const [isHidden, setHidden] = useState(false)
+    const [whiteLogo, setWhiteLogo] = useState(false)
 
-class Sidebar extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {isHidden: false, whiteLogo: false}
-    }
-
-    shouldComponentUpdate(): boolean {
-        return true
-    }
-
-    componentDidMount(): void {
-        this.updateLogo()
-    }
-
-    private updateLogo() {
+    useEffect(() => {
         const theme = loadTheme()
-        const whiteLogo = theme.sidebarWhiteLogo === 'true'
-        if (this.state.whiteLogo !== whiteLogo) {
-            this.setState({whiteLogo})
+        const newWhiteLogo = theme.sidebarWhiteLogo === 'true'
+        if (whiteLogo !== newWhiteLogo) {
+            setWhiteLogo(newWhiteLogo)
         }
+    }, [])
+
+    const {workspaceTree} = props
+    if (!workspaceTree) {
+        return <div/>
     }
 
-    render(): JSX.Element {
-        const {workspaceTree, intl} = this.props
-        if (!workspaceTree) {
-            return <div/>
-        }
+    const {boards, views} = workspaceTree
 
-        const {boards, views} = workspaceTree
-
-        if (this.state.isHidden) {
-            return (
-                <div className='Sidebar octo-sidebar hidden'>
-                    <div className='octo-sidebar-header show-button'>
-                        <div className='hamburger-icon'>
-                            <IconButton
-                                icon={<HamburgerIcon/>}
-                                onClick={this.showClicked}
-                            />
-                        </div>
-                        <div className='show-icon'>
-                            <IconButton
-                                icon={<ShowSidebarIcon/>}
-                                onClick={this.showClicked}
-                            />
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-
+    if (isHidden) {
         return (
-            <div className='Sidebar octo-sidebar'>
-                <div className='octo-sidebar-header'>
-                    <div className='heading'>
-                        <SidebarUserMenu
-                            whiteLogo={this.state.whiteLogo}
+            <div className='Sidebar octo-sidebar hidden'>
+                <div className='octo-sidebar-header show-button'>
+                    <div className='hamburger-icon'>
+                        <IconButton
+                            icon={<HamburgerIcon/>}
+                            onClick={() => setHidden(false)}
                         />
                     </div>
-
-                    <div className='octo-spacer'/>
-                    <IconButton
-                        onClick={this.hideClicked}
-                        icon={<HideSidebarIcon/>}
-                    />
+                    <div className='show-icon'>
+                        <IconButton
+                            icon={<ShowSidebarIcon/>}
+                            onClick={() => setHidden(false)}
+                        />
+                    </div>
                 </div>
-                <div className='octo-sidebar-list'>
-                    {
-                        boards.map((board) => {
-                            const nextBoardId = boards.length > 1 ? boards.find((o) => o.id !== board.id)?.id : undefined
-                            return (
-                                <SidebarBoardItem
-                                    key={board.id}
-                                    views={views}
-                                    board={board}
-                                    showBoard={this.props.showBoard}
-                                    showView={this.props.showView}
-                                    activeBoardId={this.props.activeBoardId}
-                                    nextBoardId={nextBoardId}
-                                />
-                            )
-                        })
-                    }
-                </div>
-
-                <div className='octo-spacer'/>
-
-
-                <SidebarAddBoardMenu
-                    showBoard={this.props.showBoard}
-                    workspaceTree={this.props.workspaceTree}
-                    activeBoardId={this.props.activeBoardId}
-                />
-
-                <SidebarSettingsMenu
-                    setLanguage={this.props.setLanguage}
-                    setWhiteLogo={(whiteLogo: boolean) => this.setState({whiteLogo})}
-                /> 
             </div>
         )
     }
 
-    private hideClicked = () => {
-        this.setState({isHidden: true})
-    }
+    return (
+        <div className='Sidebar octo-sidebar'>
+            <div className='octo-sidebar-header'>
+                <div className='heading'>
+                    <SidebarUserMenu whiteLogo={whiteLogo} />
+                </div>
 
-    private showClicked = () => {
-        this.setState({isHidden: false})
-    }
-}
+                <div className='octo-spacer'/>
+                <IconButton
+                    onClick={() => setHidden(true)}
+                    icon={<HideSidebarIcon/>}
+                />
+            </div>
+            <div className='octo-sidebar-list'>
+                {
+                    boards.map((board) => {
+                        const nextBoardId = boards.length > 1 ? boards.find((o) => o.id !== board.id)?.id : undefined
+                        return (
+                            <SidebarBoardItem
+                                key={board.id}
+                                views={views}
+                                board={board}
+                                showBoard={props.showBoard}
+                                showView={props.showView}
+                                activeBoardId={props.activeBoardId}
+                                nextBoardId={nextBoardId}
+                            />
+                        )
+                    })
+                }
+            </div>
 
-export default injectIntl(Sidebar)
+            <div className='octo-spacer'/>
+
+
+            <SidebarAddBoardMenu
+                showBoard={props.showBoard}
+                workspaceTree={props.workspaceTree}
+                activeBoardId={props.activeBoardId}
+            />
+
+            <SidebarSettingsMenu
+                setLanguage={props.setLanguage}
+                setWhiteLogo={(whiteLogo: boolean) => setWhiteLogo(whiteLogo)}
+            /> 
+        </div>
+    )
+})
+
+export default Sidebar
