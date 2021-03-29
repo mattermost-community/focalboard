a8d3d988817db0ed57a5bfe23424ed7732a748b9
Jesús Espino <jespinog@gmail.com>
2021-03-29 20:10:12 +0200

    Better enterprise integration

---
+++ server/einterfaces/einterfaces.go
@@ -0,0 +1,25 @@
+package einterfaces
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mattermost/focalboard/server/model"
+)
+
+type MattermostAuth interface {
+	RegisterRoutes(*mux.Router)
+	DoesUserHaveWorkspaceAccess(session *model.Session, workspaceID string) bool
+}
+
+type MattermostAuthParameters struct {
+	ServerRoot      string
+	MattermostURL   string
+	ClientID        string
+	ClientSecret    string
+	UseSecureCookie bool
+}
+
+type MattermostAuthStore interface {
+	GetUserById(userID string) (*model.User, error)
+	CreateUser(user *model.User) error
+	CreateSession(session *model.Session) error
+}
