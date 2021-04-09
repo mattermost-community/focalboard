76fda21f17b3c98f6f29625c0e9f12439d91b698
Jesús Espino <jespinog@gmail.com>
2021-04-09 18:37:02 +0200

    Improved share board component

--- webapp/src/components/shareBoardComponent.tsx
+++ webapp/src/components/shareBoardComponent.tsx
@@ -1,7 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React from 'react'
-import {injectIntl, IntlShape} from 'react-intl'
+import React, {useState, useEffect} from 'react'
+import {FormattedMessage, injectIntl, IntlShape} from 'react-intl'
+import {useRouteMatch} from 'react-router'
 
 import {ISharing} from '../blocks/sharing'
 
@@ -22,124 +23,130 @@ type Props = {
     intl: IntlShape
 }
 
-type State = {
-    sharing?: ISharing
-    wasCopied: boolean
-}
+const ShareBoardComponent = React.memo((props: Props): JSX.Element => {
+    const [wasCopied, setWasCopied] = useState(false)
+    const [sharing, setSharing] = useState<ISharing|null>(null)
+    const match = useRouteMatch<{workspaceId?: string}>()
 
-class ShareBoardComponent extends React.PureComponent<Props, State> {
-    state: State = {wasCopied: false}
-
-    componentDidMount(): void {
-        this.loadData()
+    const loadData = async () => {
+        const newSharing = await client.getSharing(props.boardId)
+        setSharing(newSharing)
+        setWasCopied(false)
     }
 
-    private async loadData() {
-        const sharing = await client.getSharing(this.props.boardId)
-        this.setState({sharing, wasCopied: false})
-    }
-
-    render(): JSX.Element {
-        const {intl} = this.props
-        const {sharing} = this.state
-
-        const isSharing = Boolean(sharing && sharing.id === this.props.boardId && sharing.enabled)
-        const readToken = (sharing && isSharing) ? sharing.token : ''
-
-        const shareUrl = new URL(window.location.toString())
-        shareUrl.searchParams.set('r', readToken)
-
-        const components = shareUrl.pathname.split('/')
-
-        // TODO: Consider passing workspaceId through props instead
-        if (components.length >= 2 && components[1].toLowerCase() === 'workspace') {
-            const workspaceId = components[2]
-            shareUrl.pathname = `/workspace/${workspaceId}/shared`
-        } else {
-            shareUrl.pathname = '/shared'
-        }
-
-        let stateDescription: string
-        if (isSharing) {
-            stateDescription = intl.formatMessage({id: 'ShareBoard.unshare', defaultMessage: 'Anyone with the link can view this board'})
-        } else {
-            stateDescription = intl.formatMessage({id: 'ShareBoard.share', defaultMessage: 'Publish to web and share this board to anyone'})
-        }
-        return (
-            <Modal
-                onClose={this.props.onClose}
-            >
-                <div className='ShareBoardComponent'>
-                    <div className='row'>
-                        <div>{stateDescription}</div>
-                        <div className='spacer'/>
-                        <Switch
-                            isOn={Boolean(isSharing)}
-                            onChanged={this.onShareChanged}
-                        />
-                    </div>
-                    {isSharing && <>
-                        <div className='row'>
-                            <a
-                                className='shareUrl'
-                                href={shareUrl.toString()}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                {shareUrl.toString()}
-                            </a>
-                            <Button
-                                filled={true}
-                                onClick={() => {
-                                    Utils.copyTextToClipboard(shareUrl.toString())
-                                    this.setState({wasCopied: true})
-                                }}
-                            >
-                                {this.state.wasCopied ? intl.formatMessage({id: 'ShareBoard.copiedLink', defaultMessage: 'Copied!'}) : intl.formatMessage({id: 'ShareBoard.copyLink', defaultMessage: 'Copy link'})}
-                            </Button>
-                        </div>
-                        <div className='row'>
-                            <Button onClick={this.onRegenerateToken}>
-                                {intl.formatMessage({id: 'ShareBoard.regenerateToken', defaultMessage: 'Regenerate token'})}
-                            </Button>
-                        </div>
-                    </>}
-                </div>
-            </Modal>
-        )
-    }
-
-    private createSharingInfo() {
-        const sharing: ISharing = {
-            id: this.props.boardId,
+    const createSharingInfo = () => {
+        const newSharing: ISharing = {
+            id: props.boardId,
             enabled: true,
             token: Utils.createGuid(),
         }
-        return sharing
+        return newSharing
     }
 
-    private onShareChanged = async (isOn: boolean) => {
-        const sharing: ISharing = this.state.sharing || this.createSharingInfo()
-        sharing.id = this.props.boardId
-        sharing.enabled = isOn
-        await client.setSharing(sharing)
-        await this.loadData()
+    const onShareChanged = async (isOn: boolean) => {
+        const newSharing: ISharing = sharing || createSharingInfo()
+        newSharing.id = props.boardId
+        newSharing.enabled = isOn
+        await client.setSharing(newSharing)
+        await loadData()
     }
 
-    private onRegenerateToken = async () => {
-        const {intl} = this.props
+    const onRegenerateToken = async () => {
+        const {intl} = props
         // eslint-disable-next-line no-alert
         const accept = window.confirm(intl.formatMessage({id: 'ShareBoard.confirmRegenerateToken', defaultMessage: 'This will invalidate previously shared links. Continue?'}))
         if (accept) {
-            const sharing: ISharing = this.state.sharing || this.createSharingInfo()
-            sharing.token = Utils.createGuid()
-            await client.setSharing(sharing)
-            await this.loadData()
+            const newSharing: ISharing = sharing || createSharingInfo()
+            newSharing.token = Utils.createGuid()
+            await client.setSharing(newSharing)
+            await loadData()
 
             const description = intl.formatMessage({id: 'ShareBoard.tokenRegenrated', defaultMessage: 'Token regenerated'})
             sendFlashMessage({content: description, severity: 'low'})
         }
     }
-}
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+    const isSharing = Boolean(sharing && sharing.id === props.boardId && sharing.enabled)
+    const readToken = (sharing && isSharing) ? sharing.token : ''
+
+    const shareUrl = new URL(window.location.toString())
+    shareUrl.searchParams.set('r', readToken)
+
+    if (match.params.workspaceId) {
+        shareUrl.pathname = `/workspace/${match.params.workspaceId}/shared`
+    } else {
+        shareUrl.pathname = '/shared'
+    }
+
+    return (
+        <Modal
+            onClose={props.onClose}
+        >
+            <div className='ShareBoardComponent'>
+                <div className='row'>
+                    <div>
+                        {isSharing &&
+                            <FormattedMessage
+                                id='ShareBoard.unshare'
+                                defaultMessage='Anyone with the link can view this board'
+                            />}
+                        {!isSharing &&
+                            <FormattedMessage
+                                id='ShareBoard.share'
+                                defaultMessage='Publish to web and share this board to anyone'
+                            />}
+                    </div>
+                    <div className='spacer'/>
+                    <Switch
+                        isOn={Boolean(isSharing)}
+                        onChanged={onShareChanged}
+                    />
+                </div>
+                {isSharing && <>
+                    <div className='row'>
+                        <a
+                            className='shareUrl'
+                            href={shareUrl.toString()}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            {shareUrl.toString()}
+                        </a>
+                        <Button
+                            filled={true}
+                            onClick={() => {
+                                Utils.copyTextToClipboard(shareUrl.toString())
+                                setWasCopied(true)
+                            }}
+                        >
+                            {wasCopied &&
+                                <FormattedMessage
+                                    id='ShareBoard.copiedLink'
+                                    defaultMessage='Copied!'
+                                />}
+                            {!wasCopied &&
+                                <FormattedMessage
+                                    id='ShareBoard.copyLink'
+                                    defaultMessage='Copy link'
+                                />}
+                        </Button>
+                    </div>
+                    <div className='row'>
+                        <Button onClick={onRegenerateToken}>
+                            <FormattedMessage
+                                id='ShareBoard.regenerateToken'
+                                defaultMessage='Regenerate token'
+                            />
+                        </Button>
+                    </div>
+                </>}
+            </div>
+        </Modal>
+    )
+})
 
 export default injectIntl(ShareBoardComponent)
