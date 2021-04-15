0a88e9606e6c41516d75b164769686b61e04707d
Chen-I Lim <chenilim@gmail.com>
2021-04-15 11:12:51 -0700

    Fix #274. Add re-login button to error page.

--- webapp/src/pages/errorPage.tsx
+++ webapp/src/pages/errorPage.tsx
@@ -1,13 +1,35 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React from 'react'
+import {FormattedMessage} from 'react-intl'
 
+import octoClient from '../octoClient'
+import Button from '../widgets/buttons/button'
 import './errorPage.scss'
 
 const ErrorPage = React.memo(() => {
     return (
         <div className='ErrorPage'>
             <div className='title'>{'Error'}</div>
+            <div>
+                <FormattedMessage
+                    id='error.no-workspace'
+                    defaultMessage='Your session may have expired or you may not have access to this workspace.'
+                />
+            </div>
+            <br/>
+            <Button
+                filled={true}
+                onClick={() => {
+                    octoClient.logout()
+                    window.location.href = '/login'
+                }}
+            >
+                <FormattedMessage
+                    id='error.relogin'
+                    defaultMessage='Log in again'
+                />
+            </Button>
         </div>
     )
 })
