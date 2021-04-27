3fb078d6123bca5a2fc76ca3edda71507c7a9866
Johannes Marbach <n0-0ne+github@mailbox.org>
2021-04-28 00:09:26 +0200

    [GH-314] Persist and reapply users settings in mac app (#331)
    * [GH-314] Persist and reapply users settings in mac app
    
    Relates to: #314
    
    * Inject settings blob at document start, push base64 conversion into TS, use proper quotes
    
    * Remove whitespace
    
    * Rename base64 to blob for consistency

--- webapp/src/userSettings.ts
+++ webapp/src/userSettings.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-class UserSettings {
+export class UserSettings {
     static get prefillRandomIcons(): boolean {
         return localStorage.getItem('randomIcons') !== 'false'
     }
@@ -11,4 +11,39 @@ class UserSettings {
     }
 }
 
-export {UserSettings}
+const keys = ['language', 'theme', 'lastBoardId', 'lastViewId', 'emoji-mart.last', 'emoji-mart.frequently']
+
+export function exportUserSettingsBlob(): string {
+    return window.btoa(exportUserSettings())
+}
+
+function exportUserSettings(): string {
+    const settings = Object.fromEntries(keys.map((key) => [key, localStorage.getItem(key)]))
+    settings.timestamp = `${Date.now()}`
+    return JSON.stringify(settings)
+}
+
+export function importUserSettingsBlob(blob: string): boolean {
+    return importUserSettings(window.atob(blob))
+}
+
+function importUserSettings(json: string): boolean {
+    const settings = parseUserSettings(json)
+    const timestamp = settings.timestamp
+    const lastTimestamp = localStorage.getItem('timestamp')
+    if (!timestamp || (lastTimestamp && Number(timestamp) <= Number(lastTimestamp))) {
+        return false
+    }
+    for (const [key, value] of Object.entries(settings)) {
+        localStorage.setItem(key, value as string)
+    }
+    return true
+}
+
+function parseUserSettings(json: string): any {
+    try {
+        return JSON.parse(json)
+    } catch (e) {
+        return {}
+    }
+}
