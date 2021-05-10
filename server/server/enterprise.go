21c5284d6d3a95ecff835c4c763cf64e9cbc3626
Harshil Sharma <harshilsharma63@gmail.com>
2021-05-10 20:45:27 +0530

    #GH-31 Configured Golangci-Lint and fixed errors (#381)
    * Moved golangci-lint config to config file
    
    * Fixed all golangci lint errors
    
    * Not allowing telemetry shutdown error to prevent store from closing
    
    * Remved golangci-lint presets

--- server/server/enterprise.go
+++ server/server/enterprise.go
@@ -4,8 +4,13 @@ import (
 	"github.com/mattermost/focalboard/server/einterfaces"
 )
 
-var mattermostAuth func(einterfaces.MattermostAuthParameters, einterfaces.MattermostAuthStore) einterfaces.MattermostAuth
+var mattermostAuth func(
+	einterfaces.MattermostAuthParameters,
+	einterfaces.MattermostAuthStore,
+) einterfaces.MattermostAuth
 
-func RegisterMattermostAuth(f func(einterfaces.MattermostAuthParameters, einterfaces.MattermostAuthStore) einterfaces.MattermostAuth) {
+func RegisterMattermostAuth(
+	f func(einterfaces.MattermostAuthParameters, einterfaces.MattermostAuthStore) einterfaces.MattermostAuth,
+) {
 	mattermostAuth = f
 }
