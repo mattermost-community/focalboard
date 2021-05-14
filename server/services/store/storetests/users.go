9ec5ba112bc78e95ac9f139e8e2358ca1c1aee0c
Harshil Sharma <harshilsharma63@gmail.com>
2021-05-14 14:47:42 +0530

    Added user store test

---
+++ server/services/store/storetests/users.go
@@ -0,0 +1,46 @@
+package storetests
+
+import (
+	"github.com/google/uuid"
+	"github.com/mattermost/focalboard/server/model"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"testing"
+
+	"github.com/mattermost/focalboard/server/services/store"
+)
+
+func StoreTestUserStore(t *testing.T, setup func(t *testing.T) (store.Store, func())) {
+	t.Run("SetGetSystemSettings", func(t *testing.T) {
+		store, tearDown := setup(t)
+		defer tearDown()
+		testGetWorkspaceUsers(t, store)
+	})
+}
+
+func testGetWorkspaceUsers(t *testing.T, store store.Store) {
+	t.Run("GetWorkspaceUSers", func(t *testing.T) {
+		users, err := store.GetUsersByWorkspace("workspace_1")
+		require.Equal(t, 0, len(users))
+		require.Nil(t, err)
+
+		userID := uuid.New().String()
+
+		err = store.CreateUser(&model.User{
+			ID:       userID,
+			Username: "darth.vader",
+		})
+		require.Nil(t, err)
+
+		defer store.UpdateUser(&model.User{
+			ID:       userID,
+			DeleteAt: time.Now().Unix(),
+		})
+
+		users, err = store.GetUsersByWorkspace("workspace_1")
+		require.Equal(t, 1, len(users))
+		require.Equal(t, "darth.vader", users[0].Username)
+		require.Nil(t, err)
+	})
+}
