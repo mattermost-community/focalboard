5052925bab255bd0bfe7ce8b671bead0f8798b04
Harshil Sharma <harshilsharma63@gmail.com>
2021-06-15 17:11:39 +0530

    Added webapp unit tests

---
+++ webapp/src/components/properties/lastModifiedAt/lastModifiedAt.test.tsx
@@ -0,0 +1,30 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react'
+import {render} from '@testing-library/react'
+
+import {CardTreeContext, MutableCardTree} from '../../../viewModel/cardTree'
+
+import {MutableCard} from '../../../blocks/card'
+
+import LastModifiedAt from './lastModifiedAt'
+
+describe('componnets/properties/lastModifiedAt', () => {
+    test('should match snapshot', () => {
+        const cardTree = new MutableCardTree(
+            new MutableCard({
+                updateAt: Date.parse('15 Jun 2021 16:22:00 +05:30'),
+            }),
+
+        )
+        const component = (
+            <CardTreeContext.Provider value={cardTree}>
+                <LastModifiedAt/>
+            </CardTreeContext.Provider>
+        )
+
+        const {container} = render(component)
+        expect(container).toMatchSnapshot()
+    })
+})
