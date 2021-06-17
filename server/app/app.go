e081a6664ca1ad323f3a2cb295627a3c780cd91f
wiggin77 <wiggin77@warpmail.net>
2021-06-17 16:25:39 -0400

    remove appbuilder

--- server/app/app.go
+++ server/app/app.go
@@ -2,15 +2,20 @@ package app
 
 import (
 	"github.com/mattermost/focalboard/server/auth"
+	"github.com/mattermost/focalboard/server/model"
 	"github.com/mattermost/focalboard/server/services/config"
 	"github.com/mattermost/focalboard/server/services/metrics"
 	"github.com/mattermost/focalboard/server/services/mlog"
 	"github.com/mattermost/focalboard/server/services/store"
 	"github.com/mattermost/focalboard/server/services/webhook"
-	"github.com/mattermost/focalboard/server/ws"
 
 	"github.com/mattermost/mattermost-server/v5/shared/filestore"
 )
+
+type WebsocketServer interface {
+	BroadcastBlockChange(workspaceID string, block model.Block)
+	BroadcastBlockDelete(workspaceID, blockID, parentID string)
+}
 
 type Services struct {
 	Auth         *auth.Auth
@@ -22,22 +27,22 @@ type Services struct {
 }
 
 type App struct {
-	config *config.Configuration
-	store  store.Store
-	auth   *auth.Auth
-	//wsServer     *ws.Server
+	config       *config.Configuration
+	store        store.Store
+	auth         *auth.Auth
+	wsServer     WebsocketServer
 	filesBackend filestore.FileBackend
 	webhook      *webhook.Client
 	metrics      *metrics.Metrics
 	logger       *mlog.Logger
 }
 
-func New(config *config.Configuration, wsServer *ws.Server, services Services) *App {
+func New(config *config.Configuration, wsServer WebsocketServer, services Services) *App {
 	return &App{
-		config: config,
-		store:  services.Store,
-		auth:   services.Auth,
-		//wsServer:     wsServer,
+		config:       config,
+		store:        services.Store,
+		auth:         services.Auth,
+		wsServer:     wsServer,
 		filesBackend: services.FilesBackend,
 		webhook:      services.Webhook,
 		metrics:      services.Metrics,
