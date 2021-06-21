60b55b4567af6c583729a3d3f234652810deb2e3
Jesús Espino <jespinog@gmail.com>
2021-06-21 08:45:23 +0200

    Reverting unneded change

--- webapp/src/widgets/valueSelector.tsx
+++ webapp/src/widgets/valueSelector.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {useState} from 'react'
+import React from 'react'
 import {useIntl} from 'react-intl'
 import {ActionMeta, FormatOptionLabelMeta, ValueType} from 'react-select'
 import CreatableSelect from 'react-select/creatable'
