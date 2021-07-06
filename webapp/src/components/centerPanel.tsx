7d1a4f1393f58779248a390c1d3526413c836162
Arvin <arvin.darmawan@gmail.com>
2021-07-06 15:00:25 +0700

    feat: open card after duplicate (#674)
    Co-authored-by: arvinWahyoo <arvin@wahyoo.com>

--- webapp/src/components/centerPanel.tsx
+++ webapp/src/components/centerPanel.tsx
@@ -155,6 +155,7 @@ class CenterPanel extends React.Component<Props, State> {
                     readonly={this.props.readonly}
                     onCardClicked={this.cardClicked}
                     addCard={this.addCard}
+                    showCard={this.showCard}
                 />}
 
                 {activeView.viewType === 'table' &&
