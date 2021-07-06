702b4b106150f13f513838e26db4e17a403dbfd8
Jesús Espino <jespinog@gmail.com>
2021-07-06 19:53:54 +0200

    Only allowing valid color classes (#665)
    * Only allowing valid color classes
    
    * Simplifying the menuColors map and addressing some PR review comments
    
    * Fixing type problems
    
    * Fixing color
    
    * Fixing snapshots

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -2,18 +2,18 @@
 // See LICENSE.txt for license information.
 
 class Constants {
-    static readonly menuColors = [
-        {id: 'propColorDefault', name: 'Default', type: 'color'},
-        {id: 'propColorGray', name: 'Gray', type: 'color'},
-        {id: 'propColorBrown', name: 'Brown', type: 'color'},
-        {id: 'propColorOrange', name: 'Orange', type: 'color'},
-        {id: 'propColorYellow', name: 'Yellow', type: 'color'},
-        {id: 'propColorGreen', name: 'Green', type: 'color'},
-        {id: 'propColorBlue', name: 'Blue', type: 'color'},
-        {id: 'propColorPurple', name: 'Purple', type: 'color'},
-        {id: 'propColorPink', name: 'Pink', type: 'color'},
-        {id: 'propColorRed', name: 'Red', type: 'color'},
-    ]
+    static readonly menuColors: {[key: string]: string} = {
+        propColorDefault: 'Default',
+        propColorGray: 'Gray',
+        propColorBrown: 'Brown',
+        propColorOrange: 'Orange',
+        propColorYellow: 'Yellow',
+        propColorGreen: 'Green',
+        propColorBlue: 'Blue',
+        propColorPurple: 'Purple',
+        propColorPink: 'Pink',
+        propColorRed: 'Red',
+    }
 
     static readonly minColumnWidth = 100
     static readonly defaultTitleColumnWidth = 280
