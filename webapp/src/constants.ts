9624a5ede9873dfcfbeace2ff51845d187736421
Miguel de la Cruz <miguel@mcrx.me>
2021-07-07 13:48:53 +0200

    Update version to v0.7.3

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -19,7 +19,7 @@ class Constants {
     static readonly defaultTitleColumnWidth = 280
     static readonly titleColumnId = '__title'
 
-    static readonly versionString = '0.7.2'
+    static readonly versionString = '0.7.3'
 }
 
 export {Constants}
