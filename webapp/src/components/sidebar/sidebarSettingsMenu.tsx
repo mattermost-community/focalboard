e438a61c21305686d30234ce3980223c7affd7c0
Jesús Espino <jespinog@gmail.com>
2021-07-08 10:15:37 +0200

    Removing unused imports

--- webapp/src/components/sidebar/sidebarSettingsMenu.tsx
+++ webapp/src/components/sidebar/sidebarSettingsMenu.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {useContext, useState} from 'react'
+import React, {useState} from 'react'
 import {FormattedMessage, useIntl} from 'react-intl'
 
 import {Archiver} from '../../archiver'
--- webapp/src/user.tsx
+++ webapp/src/user.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react'
-
 interface IUser {
     id: string,
     username: string,
