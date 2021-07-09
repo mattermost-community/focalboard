64ed5640bed253815aff08d13c95f2bff5fd7a92
Andrey Eremchenko <eremchenko@gmail.com>
2021-07-09 23:52:45 +0700

    Cypress tests for login actions:
      - function for checking that workspace is available added,
      - functions for login and logout added,
      - test for password change added,
      - session parameters added to server config for cypress testing.

--- webapp/cypress/integration/loginActions.js
+++ webapp/cypress/integration/loginActions.js
@@ -5,6 +5,26 @@ describe('Login actions', () => {
     const username = 'username';
     const email = 'username@gmail.com';
     const password = 'password';
+    const newPassword = 'new_password';
+
+    const workspaceIsAvailable = () => {
+        cy.get('.Workspace').should('exist');
+        cy.get('.Sidebar').should('exist');
+    };
+
+    const loginWithPassword = (withPassword) => {
+        cy.visit('/login');
+        cy.get('input[placeholder="Enter username"').type(username);
+        cy.get('input[placeholder="Enter password"').type(withPassword);
+        cy.get('button').contains('Log in').click();
+        workspaceIsAvailable();
+    };
+
+    const logout = () => {
+        cy.get('.Sidebar .SidebarUserMenu').click();
+        cy.get('.Menu .MenuOption .menu-name').contains('Log out').click();
+        cy.location('pathname').should('eq', '/login');
+    };
 
     it('Redirects to login page', () => {
         cy.visit('/');
@@ -17,6 +37,7 @@ describe('Login actions', () => {
     });
 
     it('Can register user', () => {
+        cy.visit('/login');
         cy.get('a').contains('create an account').click();
         cy.location('pathname').should('eq', '/register');
         cy.get('.RegisterPage').contains('Sign up');
@@ -24,14 +45,25 @@ describe('Login actions', () => {
         cy.get('input[placeholder="Enter username"').type(username);
         cy.get('input[placeholder="Enter password"').type(password);
         cy.get('button').contains('Register').click();
-        cy.get('.Workspace').should('exist');
-        cy.get('.Sidebar').should('exist');
+        workspaceIsAvailable();
+        logout();
     });
 
-    it('Can login registered user', () => {
-        cy.visit('/login');
-        cy.get('input[placeholder="Enter username"').type(username);
-        cy.get('input[placeholder="Enter password"').type(password);
-        cy.get('button').contains('Log in').click();
+    it('Can change password', () => {
+        loginWithPassword(password);
+        cy.get('.Sidebar .SidebarUserMenu').click();
+        cy.get('.Menu .MenuOption .menu-name').contains('Change password').click();
+        cy.location('pathname').should('eq', '/change_password');
+        cy.get('.ChangePasswordPage').contains('Change Password');
+        cy.get('input[placeholder="Enter current password"').type(password);
+        cy.get('input[placeholder="Enter new password"').type(newPassword);
+        cy.get('button').contains('Change password').click();
+        cy.get('.succeeded').click();
+        workspaceIsAvailable();
+        logout();
+    });
+
+    it('Can log in user with new password', () => {
+        loginWithPassword(newPassword);
     });
 });
