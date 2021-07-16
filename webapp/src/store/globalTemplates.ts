7134c732b6cf8cc233f5b0358b040399760be568
Jesús Espino <jespinog@gmail.com>
2021-07-16 13:31:01 +0200

    Fixing eslint

--- webapp/src/store/globalTemplates.ts
+++ webapp/src/store/globalTemplates.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {createSlice, createAsyncThunk, PayloadAction} from '@reduxjs/toolkit'
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
 import {default as client, OctoClient} from '../octoClient'
 import {MutableBoard} from '../blocks/board'
