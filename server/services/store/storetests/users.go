8b25b365f1f009aab39c14ddddb6f1d07111e775
yedamao <logindaveye@gmail.com>
2021-07-26 23:01:29 +0800

    Add more unit tests for ./store/sqlstore/user.go

--- server/services/store/storetests/users.go
+++ server/services/store/storetests/users.go
@@ -5,15 +5,13 @@ package storetests
 
 import (
 	"database/sql"
+	"testing"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/mattermost/focalboard/server/model"
-
-	"testing"
-
 	"github.com/stretchr/testify/require"
 
+	"github.com/mattermost/focalboard/server/model"
 	"github.com/mattermost/focalboard/server/services/store"
 )
 
@@ -22,6 +20,24 @@ func StoreTestUserStore(t *testing.T, setup func(t *testing.T) (store.Store, fun
 		store, tearDown := setup(t)
 		defer tearDown()
 		testGetWorkspaceUsers(t, store)
+	})
+
+	t.Run("CreateAndGetUser", func(t *testing.T) {
+		store, tearDown := setup(t)
+		defer tearDown()
+		testCreateAndGetUser(t, store)
+	})
+
+	t.Run("CreateAndUpateUser", func(t *testing.T) {
+		store, tearDown := setup(t)
+		defer tearDown()
+		testCreateAndUpdateUser(t, store)
+	})
+
+	t.Run("CreateAndGetRegisteredUserCount", func(t *testing.T) {
+		store, tearDown := setup(t)
+		defer tearDown()
+		testCreateAndGetRegisteredUserCount(t, store)
 	})
 }
 
@@ -51,4 +67,100 @@ func testGetWorkspaceUsers(t *testing.T, store store.Store) {
 		require.Equal(t, "darth.vader", users[0].Username)
 		require.Nil(t, err)
 	})
+}
+
+func testCreateAndGetUser(t *testing.T, store store.Store) {
+	user := &model.User{
+		ID:       uuid.New().String(),
+		Username: "damao",
+		Email:    "mock@email.com",
+	}
+
+	t.Run("CreateUser", func(t *testing.T) {
+		err := store.CreateUser(user)
+		require.Nil(t, err)
+	})
+
+	t.Run("GetUserByID", func(t *testing.T) {
+		got, err := store.GetUserByID(user.ID)
+		require.Nil(t, err)
+		require.Equal(t, user.ID, got.ID)
+		require.Equal(t, user.Username, got.Username)
+		require.Equal(t, user.Email, got.Email)
+	})
+
+	t.Run("GetUserByUsername", func(t *testing.T) {
+		got, err := store.GetUserByUsername(user.Username)
+		require.Nil(t, err)
+		require.Equal(t, user.ID, got.ID)
+		require.Equal(t, user.Username, got.Username)
+		require.Equal(t, user.Email, got.Email)
+	})
+
+	t.Run("GetUserByEmail", func(t *testing.T) {
+		got, err := store.GetUserByEmail(user.Email)
+		require.Nil(t, err)
+		require.Equal(t, user.ID, got.ID)
+		require.Equal(t, user.Username, got.Username)
+		require.Equal(t, user.Email, got.Email)
+	})
+}
+
+func testCreateAndUpdateUser(t *testing.T, store store.Store) {
+	user := &model.User{
+		ID: uuid.New().String(),
+	}
+	err := store.CreateUser(user)
+	require.Nil(t, err)
+
+	t.Run("UpdateUser", func(t *testing.T) {
+		user.Username = "damao"
+		user.Email = "mock@email.com"
+		user.Props = map[string]interface{}{"a": "b"}
+		err := store.UpdateUser(user)
+		require.Nil(t, err)
+
+		got, err := store.GetUserByID(user.ID)
+		require.Nil(t, err)
+		require.Equal(t, user.ID, got.ID)
+		require.Equal(t, user.Username, got.Username)
+		require.Equal(t, user.Email, got.Email)
+		require.Equal(t, user.Props, got.Props)
+	})
+
+	t.Run("UpdateUserPassword", func(t *testing.T) {
+		newPassword := uuid.New().String()
+		err := store.UpdateUserPassword(user.Username, newPassword)
+		require.Nil(t, err)
+
+		got, err := store.GetUserByUsername(user.Username)
+		require.Nil(t, err)
+		require.Equal(t, user.Username, got.Username)
+		require.Equal(t, newPassword, got.Password)
+	})
+
+	t.Run("UpdateUserPasswordByID", func(t *testing.T) {
+		newPassword := uuid.New().String()
+		err := store.UpdateUserPasswordByID(user.ID, newPassword)
+		require.Nil(t, err)
+
+		got, err := store.GetUserByID(user.ID)
+		require.Nil(t, err)
+		require.Equal(t, user.ID, got.ID)
+		require.Equal(t, newPassword, got.Password)
+	})
+}
+
+func testCreateAndGetRegisteredUserCount(t *testing.T, store store.Store) {
+	randomN := int(time.Now().Unix() % 10)
+	for i := 0; i < randomN; i++ {
+		err := store.CreateUser(&model.User{
+			ID: uuid.New().String(),
+		})
+		require.Nil(t, err)
+	}
+
+	got, err := store.GetRegisteredUserCount()
+	require.Nil(t, err)
+	require.Equal(t, randomN, got)
 }
