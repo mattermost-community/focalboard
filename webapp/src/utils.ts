4fcf1cc50deda825a02340bee596e1a426ff85d1
kamre <eremchenko@gmail.com>
2021-07-27 16:11:18 +0700

    [GH-779] Consistent date time format (#791)
    * Use `Utils.displayDateTime` in Comment/CreatedAt/LastModifiedAt components.
    Option for month changed to 'long' for `Utils.displayDateTime`.
    
    * Don't show current year in `displayDateTime`.
    
    Co-authored-by: Harshil Sharma <harshilsharma63@gmail.com>

--- webapp/src/utils.ts
+++ webapp/src/utils.ts
@@ -121,24 +121,27 @@ class Utils {
 
     // Date and Time
 
-    static displayDate(date: Date, intl: IntlShape): string {
+    private static yearOption(date: Date) {
         const isCurrentYear = date.getFullYear() === new Date().getFullYear()
+        return isCurrentYear ? undefined : 'numeric'
+    }
+
+    static displayDate(date: Date, intl: IntlShape): string {
         return intl.formatDate(date, {
-            year: isCurrentYear ? undefined : 'numeric',
+            year: Utils.yearOption(date),
             month: 'long',
             day: '2-digit',
         })
     }
 
     static displayDateTime(date: Date, intl: IntlShape): string {
-        const text = intl.formatDate(date, {
-            year: 'numeric',
-            month: 'short',
+        return intl.formatDate(date, {
+            year: Utils.yearOption(date),
+            month: 'long',
             day: '2-digit',
             hour: 'numeric',
             minute: 'numeric',
         })
-        return text
     }
 
     static sleep(miliseconds: number): Promise<void> {
