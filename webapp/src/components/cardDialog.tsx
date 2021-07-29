9015fa0dedc3cbc05a30a5e8daf5c41ba0f99b4a
Jesús Espino <jespinog@gmail.com>
2021-07-29 13:34:08 +0200

    Fix eslint

--- webapp/src/components/cardDialog.tsx
+++ webapp/src/components/cardDialog.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {useState, useEffect, useCallback} from 'react'
+import React, {useState, useEffect} from 'react'
 import {FormattedMessage, useIntl} from 'react-intl'
 
 import mutator from '../mutator'
