515edb9dcfe26fdfb776d6b64fa7fff5dea009a5
Christopher Speller <crspeller@gmail.com>
2021-08-04 07:57:56 -0700

    Global header support (#736)
    * Adding global header support.
    
    * Set the landing URL for the global header to /dashboard
    
    * Redirecting directly to the current channel on open Boards
    
    * Some improvements on global header
    
    * Adding missed file
    
    * Removing theme selector from the global header and fixing a style bug on the channel header icon
    
    * Adding global header tests
    
    * Adding missed file
    
    * Fix global header icon
    
    * Adding isFocalboardPlugin function
    
    * Fixing unit tests
    
    * Fixing problem with header menu styles
    
    Co-authored-by: Jesús Espino <jespinog@gmail.com>

--- mattermost-plugin/webapp/src/types/mattermost-webapp/index.d.ts
+++ mattermost-plugin/webapp/src/types/mattermost-webapp/index.d.ts
@@ -4,8 +4,9 @@ export interface PluginRegistry {
     registerPostTypeComponent(typeName: string, component: React.ElementType)
     registerChannelHeaderButtonAction(icon: React.Element, action: () => void, dropdownText: string, tooltipText: string)
     registerCustomRoute(route: string, component: React.ElementType)
+    registerProductRoute(route: string, component: React.ElementType)
     unregisterComponent(componentId: string)
-    registerNeedsTeamRoute(route: string, component: React.ElementType)
+    registerProduct(baseURL: string, switcherIcon: React.ElementType, switcherText: string, switcherLinkURL: string, mainComponent: React.ElementType, headerCompoent: React.ElementType)
 
     // Add more if needed from https://developers.mattermost.com/extend/plugins/webapp/reference
 }
--- webapp/src/widgets/icons/settings.tsx
+++ webapp/src/widgets/icons/settings.tsx
@@ -1,16 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+
 import React from 'react'
 
 import CompassIcon from './compassIcon'
 
-import './help.scss'
+import './settings.scss'
 
-export default function HelpIcon(): JSX.Element {
+export default function SettingsIcon(): JSX.Element {
     return (
         <CompassIcon
-            icon='help-circle-outline'
-            className='HelpIcon'
+            icon='settings-outline'
+            className='SettingsIcon'
         />
     )
 }
