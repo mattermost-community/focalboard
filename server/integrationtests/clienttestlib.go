15b5d9746f3d394e459dc1d55987d3ef4a0b7ed7
dave <logindaveye@gmail.com>
2021-08-10 10:57:45 +0800

    [GH-436] Add integration tests for missing User API endpoints (#810)
    * server/client: support register and login
    
    * server/client: support user related apis
    
    * integrationtests: Add SetupTestHelperWithoutToken
    
    * Add api integration tests for (User APIs)
    
    * rename GetUserMe method to GetMe
    
    * check GetMe data is match the registered data after login
    
    * Add integration test for workspace upload file api
    
    * make ci happy

--- server/integrationtests/clienttestlib.go
+++ server/integrationtests/clienttestlib.go
@@ -50,21 +50,21 @@ func getTestConfig() *config.Configuration {
 	}`
 
 	return &config.Configuration{
-		ServerRoot:     "http://localhost:8888",
-		Port:           8888,
-		DBType:         dbType,
-		DBConfigString: connectionString,
-		DBTablePrefix:  "test_",
-		WebPath:        "./pack",
-		FilesDriver:    "local",
-		FilesPath:      "./files",
-		LoggingCfgJSON: logging,
+		ServerRoot:        "http://localhost:8888",
+		Port:              8888,
+		DBType:            dbType,
+		DBConfigString:    connectionString,
+		DBTablePrefix:     "test_",
+		WebPath:           "./pack",
+		FilesDriver:       "local",
+		FilesPath:         "./files",
+		LoggingCfgJSON:    logging,
+		SessionExpireTime: int64(30 * time.Second),
+		AuthMode:          "native",
 	}
 }
 
-func SetupTestHelper() *TestHelper {
-	sessionToken := "TESTTOKEN"
-	th := &TestHelper{}
+func newTestServer(singleUserToken string) *server.Server {
 	logger := mlog.NewLogger()
 	if err := logger.Configure("", getTestConfig().LoggingCfgJSON); err != nil {
 		panic(err)
@@ -74,13 +74,26 @@ func SetupTestHelper() *TestHelper {
 	if err != nil {
 		panic(err)
 	}
-	srv, err := server.New(cfg, sessionToken, db, logger, "")
+	srv, err := server.New(cfg, singleUserToken, db, logger, "")
 	if err != nil {
 		panic(err)
 	}
-	th.Server = srv
-	th.Client = client.NewClient(srv.Config().ServerRoot, sessionToken)
 
+	return srv
+}
+
+func SetupTestHelper() *TestHelper {
+	sessionToken := "TESTTOKEN"
+	th := &TestHelper{}
+	th.Server = newTestServer(sessionToken)
+	th.Client = client.NewClient(th.Server.Config().ServerRoot, sessionToken)
+	return th
+}
+
+func SetupTestHelperWithoutToken() *TestHelper {
+	th := &TestHelper{}
+	th.Server = newTestServer("")
+	th.Client = client.NewClient(th.Server.Config().ServerRoot, "")
 	return th
 }
 
@@ -124,4 +137,6 @@ func (th *TestHelper) TearDown() {
 	if err != nil {
 		panic(err)
 	}
+
+	os.RemoveAll(th.Server.Config().FilesPath)
 }
