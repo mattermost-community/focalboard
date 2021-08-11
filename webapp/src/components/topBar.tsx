9a8b19c4de4b6189a347eac229ff6d5b13cb2c4d
Chen-I Lim <chenilim@gmail.com>
2021-08-11 15:43:19 -0700

    Move version label to top right for plugin

--- webapp/src/components/topBar.tsx
+++ webapp/src/components/topBar.tsx
@@ -6,11 +6,24 @@ import React from 'react'
 import './topBar.scss'
 import HelpIcon from '../widgets/icons/help'
 import {Utils} from '../utils'
+import {Constants} from '../constants'
 
 const TopBar = React.memo((): JSX.Element => {
     if (Utils.isFocalboardPlugin()) {
-        return <></>
+        return (
+            <div
+                className='TopBar'
+            >
+                <div className='versionFrame'>
+                    <div className='version'>
+                        {`v${Constants.versionString}`}
+                    </div>
+                    <div className='versionBadge'>{'BETA'}</div>
+                </div>
+            </div>
+        )
     }
+
     return (
         <div
             className='TopBar'
