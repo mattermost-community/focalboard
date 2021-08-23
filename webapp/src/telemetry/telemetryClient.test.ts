f61c1ea760842b89effe4dd896ae76407fd239ae
Scott Bishel <scott.bishel@mattermost.com>
2021-08-23 11:29:50 -0600

    add test, update filename

---
+++ webapp/src/telemetry/telemetryClient.test.ts
@@ -0,0 +1,33 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import '@testing-library/jest-dom'
+
+import TelemetryClient from './telemetryClient'
+
+jest.mock('rudder-sdk-js', () => {
+    const original = jest.requireActual('rudder-sdk-js')
+
+    return {
+        ...original,
+        track: jest.fn(),
+    }
+})
+
+describe('trackEvent', () => {
+    const track = jest.fn()
+    const page = jest.fn()
+    test('should call Rudder\'s track when a RudderTelemetryHandler is attached to TelemetryClient', () => {
+        TelemetryClient.setTelemetryHandler()
+        TelemetryClient.trackEvent('test', 'onClick')
+        TelemetryClient.pageVisited('focalboard', 'test')
+        expect(track).not.toHaveBeenCalled()
+        expect(page).not.toHaveBeenCalled()
+
+        TelemetryClient.setTelemetryHandler({trackEvent: track, pageVisited: page})
+        TelemetryClient.trackEvent('test', 'onClick')
+        TelemetryClient.pageVisited('focalboard', 'test')
+
+        expect(track).toHaveBeenCalledTimes(1)
+        expect(page).toHaveBeenCalledTimes(1)
+    })
+})
