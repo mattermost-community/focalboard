a7b8b066932ce7d8b5d0a8c5311bfd01197fe300
Jesús Espino <jespinog@gmail.com>
2021-08-27 13:29:33 +0200

    Adding the board memberships creation

--- server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.down.sql
+++ server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE {{.prefix}}blocks DROP COLUMN team_id VARCHAR(36);
 ALTER TABLE {{.prefix}}blocks RENAME COLUMN channel_id TO workspace_id;
 ALTER TABLE {{.prefix}}teams RENAME TO {{.prefix}}workspaces;
+DROP TABLE {{.prefix}}board_members
--- server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.up.sql
+++ server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.up.sql
@@ -3,6 +3,24 @@ ALTER TABLE {{.prefix}}blocks RENAME COLUMN workspace_id TO channel_id;
 ALTER TABLE {{.prefix}}blocks ADD COLUMN team_id VARCHAR(36);
 
 {{if .plugin}}
+CREATE TABLE {{.prefix}board_members (
+    boardId character varying(26) NOT NULL,
+    userId character varying(26) NOT NULL,
+    roles character varying(64),
+    schemeAdmin boolean,
+    schemeEditor boolean,
+    schemeCommenter boolean,
+    schemeViewer boolean,
+    lastviewedat bigint,
+    mentioncount bigint,
+    notifyprops jsonb,
+	PRIMARY KEY (boardId, userId)
+);
+
+CREATE INDEX idx_boardmembers_user_id ON {{.prefix}}board_members(userId);
+
+INSERT INTO {{.prefix}}board_members (SELECT B.Id, CM.UserId, CM.SchemeAdmin, CM.SchemeUser, CM.SchemeGuest, FALSE, 0, 0, '{}' FROM {{.prefix}}blocks AS B WHERE B.type='board' INNER JOIN ChannelMembers as CM ON CM.ChannelId=B.channel_id);
+
 {{if .mysql}}
 UPDATE {{.prefix}}blocks as B, Channels as C ON C.Id=B.channel_id SET team_id = C.teamId;
 {{end}}
