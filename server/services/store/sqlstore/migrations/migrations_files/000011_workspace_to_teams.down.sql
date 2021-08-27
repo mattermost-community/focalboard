6e8e3ff28482e85deb3f940a22197151c1e535f8
Jesús Espino <jespinog@gmail.com>
2021-08-27 18:41:15 +0200

    Initial idea of boards as table

--- server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.down.sql
+++ server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.down.sql
@@ -1,4 +1,11 @@
-ALTER TABLE {{.prefix}}blocks DROP COLUMN team_id VARCHAR(36);
+ALTER TABLE {{.prefix}}blocks DROP COLUMN board_id VARCHAR(36);
 ALTER TABLE {{.prefix}}blocks RENAME COLUMN channel_id TO workspace_id;
+ALTER TABLE {{.prefix}}blocks_history DROP COLUMN board_id VARCHAR(36);
+ALTER TABLE {{.prefix}}blocks_history RENAME COLUMN channel_id TO workspace_id;
 ALTER TABLE {{.prefix}}teams RENAME TO {{.prefix}}workspaces;
+
+/* TODO: Re-create the board blocks from the boards table */ 
+/* INSERT INTO {{.prefix}}blocks (SELECT ... FROM {{.prefix}}boards AS B) */
+
 DROP TABLE {{.prefix}}board_members
+DROP TABLE {{.prefix}}boards
--- server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.up.sql
+++ server/services/store/sqlstore/migrations/migrations_files/000011_workspace_to_teams.up.sql
@@ -1,37 +1,98 @@
 ALTER TABLE {{.prefix}}workspaces RENAME TO {{.prefix}}teams;
 ALTER TABLE {{.prefix}}blocks RENAME COLUMN workspace_id TO channel_id;
-ALTER TABLE {{.prefix}}blocks ADD COLUMN team_id VARCHAR(36);
+ALTER TABLE {{.prefix}}blocks ADD COLUMN board_id VARCHAR(26);
+ALTER TABLE {{.prefix}}blocks_history RENAME COLUMN workspace_id TO channel_id;
+ALTER TABLE {{.prefix}}blocks_history ADD COLUMN board_id VARCHAR(26);
+
+{{if .mysql}}
+ALTER TABLE {{.prefix}}users CHANGE props props JSON;
+ALTER TABLE {{.prefix}}sessions CHANGE props props JSON;
+ALTER TABLE {{.prefix}}blocks CHANGE fields fields JSON;
+ALTER TABLE {{.prefix}}blocks_history CHANGE fields fields JSON;
+ALTER TABLE {{.prefix}}teams CHANGE settings settings JSON;
+{{end}
+
+ALTER TABLE features CHANGE feature feature JSON NOT NULL;
+CREATE TABLE {{.prefix}boards (
+    id VARCHAR(26) NOT NULL PRIMARY KEY,
+    team_id VARCHAR(26) NOT NULL,
+    channel_id VARCHAR(26),
+    creatord_id VARCHAR(26) NOT NULL,
+    title TEXT,
+    description TEXT,
+    icon VARCHAR(256),
+    type VARCHAR(1) NOT NULL,
+    show_description BOOLEAN,
+    is_template BOOLEAN,
+    {{if .mysql}}
+    properties JSON,
+    card_properties JSON,
+    column_calculations JSON,
+    {{end}}
+	{{if .postgres}},
+    properties JSONB,
+    card_properties JSONB,
+    column_calculations JSONB,
+    {{end}}
+	{{if .sqlite}},
+    properties TEXT,
+    card_properties TEXT,
+    column_calculations TEXT,
+    {{end}}
+	create_at BIGINT,
+	update_at BIGINT,
+	delete_at BIGINT,
+);
+
+INSERT INTO {{.prefix}}boards (SELECT B.Id, C.TeamId, B.channel_id, B.creator_id, B.title, B.fields.description, B.fields.icon, B.fields.description, B.fields.show_description, B.fields.is_template, '{}', B.fields.card_properties, B.fields.column_calculations, B.create_at, B.update_at, B.delete_at FROM {{.prefix}}blocks AS B INNER JOIN Channel as C ON C.Id=B.channel_id WHERE B.type='board')
+
+{{if .mysql}}
+UPDATE {{.prefix}}blocks as B, {{.prefix}}blocks as P ON P.Id=B.parent_id SET B.board_id=P.id WHERE P.type = 'board';
+UPDATE {{.prefix}}blocks as B, {{.prefix}}blocks as P ON P.Id=B.parent_id, {{.prefix}}blocks as GP ON GP.Id=P.parent_id SET B.board_id=GP.id WHERE GP.type = 'board';
+UPDATE {{.prefix}}blocks as B, {{.prefix}}blocks as P ON P.Id=B.parent_id, {{.prefix}}blocks as GP ON GP.Id=P.parent_id, {{.prefix}}blocks as GGP ON GGP.Id=GP.parent_id SET B.board_id=GGP.id WHERE GGP.type = 'board';
+UPDATE {{.prefix}}blocks_history as B, {{.prefix}}blocks_history as P ON P.Id=B.parent_id SET B.board_id=P.id WHERE P.type = 'board';
+UPDATE {{.prefix}}blocks_history as B, {{.prefix}}blocks_history as P ON P.Id=B.parent_id, {{.prefix}}blocks_history as GP ON GP.Id=P.parent_id SET B.board_id=GP.id WHERE GP.type = 'board';
+UPDATE {{.prefix}}blocks_history as B, {{.prefix}}blocks_history as P ON P.Id=B.parent_id, {{.prefix}}blocks_history as GP ON GP.Id=P.parent_id, {{.prefix}}blocks_history as GGP ON GGP.Id=GP.parent_id SET B.board_id=GGP.id WHERE GGP.type = 'board';
+{{end}}
+{{if .sqlite}}
+UPDATE {{.prefix}}blocks as B, {{.prefix}}blocks as P ON P.Id=B.parent_id SET B.board_id=P.id WHERE P.type = 'board';
+UPDATE {{.prefix}}blocks as B, {{.prefix}}blocks as P ON P.Id=B.parent_id, {{.prefix}}blocks as GP ON GP.Id=P.parent_id SET B.board_id=GP.id WHERE GP.type = 'board';
+UPDATE {{.prefix}}blocks as B, {{.prefix}}blocks as P ON P.Id=B.parent_id, {{.prefix}}blocks as GP ON GP.Id=P.parent_id, {{.prefix}}blocks as GGP ON GGP.Id=GP.parent_id SET B.board_id=GGP.id WHERE GGP.type = 'board';
+UPDATE {{.prefix}}blocks_history as B, {{.prefix}}blocks_history as P ON P.Id=B.parent_id SET B.board_id=P.id WHERE P.type = 'board';
+UPDATE {{.prefix}}blocks_history as B, {{.prefix}}blocks_history as P ON P.Id=B.parent_id, {{.prefix}}blocks_history as GP ON GP.Id=P.parent_id SET B.board_id=GP.id WHERE GP.type = 'board';
+UPDATE {{.prefix}}blocks_history as B, {{.prefix}}blocks_history as P ON P.Id=B.parent_id, {{.prefix}}blocks_history as GP ON GP.Id=P.parent_id, {{.prefix}}blocks_history as GGP ON GGP.Id=GP.parent_id SET B.board_id=GGP.id WHERE GGP.type = 'board';
+{{end}}
+{{if .postgres}}
+UPDATE {{.prefix}}blocks as B SET B.board_id=P.id FROM {{.prefix}}blocks as P WHERE P.Id=B.parent_id AND P.type = 'board';
+UPDATE {{.prefix}}blocks as B SET B.board_id=GP.id FROM {{.prefix}}blocks as P {{.prefix}}blocks as GP WHERE P.Id=B.parent_id AND GP.Id=P.parent_id AND GP.type = 'board';
+UPDATE {{.prefix}}blocks as B SET B.board_id=GGP.id FROM {{.prefix}}blocks as P, {{.prefix}}blocks as GP, {{.prefix}}blocks as GGP WHERE P.Id=B.parent_id AND GP.Id=P.parent_id AND GGP.Id=GP.parent_id AND GGP.type = 'board';
+UPDATE {{.prefix}}blocks_history as B SET B.board_id=P.id FROM {{.prefix}}blocks_history as P WHERE P.Id=B.parent_id AND P.type = 'board';
+UPDATE {{.prefix}}blocks_history as B SET B.board_id=GP.id FROM {{.prefix}}blocks_history as P {{.prefix}}blocks_history as GP WHERE P.Id=B.parent_id AND GP.Id=P.parent_id AND GP.type = 'board';
+UPDATE {{.prefix}}blocks_history as B SET B.board_id=GGP.id FROM {{.prefix}}blocks_history as P, {{.prefix}}blocks_history as GP, {{.prefix}}blocks_history as GGP WHERE P.Id=B.parent_id AND GP.Id=P.parent_id AND GGP.Id=GP.parent_id AND GGP.type = 'board';
+{{end}}
+
+DELETE FROM {{.prefix}}blocks WHERE type='board';
 
 {{if .plugin}}
 CREATE TABLE {{.prefix}board_members (
-    boardId character varying(26) NOT NULL,
-    userId character varying(26) NOT NULL,
-    roles character varying(64),
-    schemeAdmin boolean,
-    schemeEditor boolean,
-    schemeCommenter boolean,
-    schemeViewer boolean,
+    board_id VARCHAR(26) NOT NULL,
+    user_id VARCHAR(26) NOT NULL,
+    roles VARCHAR(64),
+    scheme_admin boolean,
+    scheme_editor boolean,
+    scheme_commenter boolean,
+    scheme_viewer boolean,
     lastviewedat bigint,
-    mentioncount bigint,
+    mention_count bigint,
     {{if .mysql}}
-    notifyprops json,
+    notify_props json,
     {{else}}
-    notifyprops jsonb,
+    notify_props jsonb,
     {{end}}
-    notifyprops jsonb,
 	PRIMARY KEY (boardId, userId)
 );
 
 CREATE INDEX idx_boardmembers_user_id ON {{.prefix}}board_members(userId);
 
 INSERT INTO {{.prefix}}board_members (SELECT B.Id, CM.UserId, CM.SchemeAdmin, CM.SchemeUser, CM.SchemeGuest, FALSE, 0, 0, '{}' FROM {{.prefix}}blocks AS B WHERE B.type='board' INNER JOIN ChannelMembers as CM ON CM.ChannelId=B.channel_id);
-
-{{if .mysql}}
-UPDATE {{.prefix}}blocks as B, Channels as C ON C.Id=B.channel_id SET team_id = C.teamId;
-{{end}}
-{{if .postgres}}
-UPDATE {{.prefix}}blocks as B SET team_id = C.teamId FROM Channels as C WHERE C.Id=B.channel_id;
-{{end}}
-{{else}}
-UPDATE {{.prefix}}blocks as B SET team_id = '0';
 {{end}}
