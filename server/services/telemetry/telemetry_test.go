d99ee7a3201b9c6b2b3bc8e5637ba356120750c3
Scott Bishel <scott.bishel@mattermost.com>
2021-08-30 16:27:38 -0600

    Update server/services/telemetry/telemetry_test.go

--- server/services/telemetry/telemetry_test.go
+++ server/services/telemetry/telemetry_test.go
@@ -60,7 +60,7 @@ func TestTelemetry(t *testing.T) {
 	}
 
 	t.Run("Register tracker and run telemetry job", func(t *testing.T) {
-		service := New("mockTelemetryID", mlog.CreateConsoleTestLogger(t))
+		service := New("mockTelemetryID", mlog.CreateConsoleTestLogger(false, mlog.LvlDebug))
 		service.RegisterTracker("mockTracker", func() (Tracker, error) {
 			return map[string]interface{}{
 				"mockTrackerKey": "mockTrackerValue",
