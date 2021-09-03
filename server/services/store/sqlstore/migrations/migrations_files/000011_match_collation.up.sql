2169a97d469189bf648addad1d8a35b1e9bbe01c
Harshil Sharma <harshilsharma63@gmail.com>
2021-09-03 13:22:28 +0530

    Handled the case of personal server running on MySQL

--- server/services/store/sqlstore/migrations/migrations_files/000011_match_collation.up.sql
+++ server/services/store/sqlstore/migrations/migrations_files/000011_match_collation.up.sql
@@ -1,44 +1,57 @@
 {{if .mysql}}
-SET @targetCollation = (SELECT table_collation from information_schema.tables WHERE table_name = 'Channels') AND table_schema = (SELECT DATABASE());
+-- collation of mattermost's Channels table
+SET @mattermostCollation = (SELECT table_collation from information_schema.tables WHERE table_name = 'Channels' AND table_schema = (SELECT DATABASE()));
+
+-- collation of Focalboard's blocks table
+SET @focalboardCollation = (SELECT table_collation from information_schema.tables WHERE table_name = '{{.prefix}}blocks' AND table_schema = (SELECT DATABASE()));
+
+-- Uses mattermost's collation if available.
+--
+-- This is needed to support running Focalboard in personal server mode,
+-- where no Channels table will exists.
+-- Wi still update the table collations in personal server mode
+-- to avoid the complexities of if-else conditions in MySQL queries,
+-- which are not 100% consistent across versions and are a trickier to debug.
+SET @targetCollation = COALESCE(@mattermostCollation, @focalboardCollation);
 
 -- blocks
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_blocks COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}blocks COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- blocks history
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_blocks_history COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}blocks_history COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- sessions
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_sessions COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}sessions COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- sharing
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_sharing COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}sharing COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- system settings
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_system_settings COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}system_settings COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- users
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_users COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}users COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- workspaces
-SET @updateCollationQuery = CONCAT('ALTER TABLE focalboard_workspaces COLLATE ', @targetCollation);
+SET @updateCollationQuery = CONCAT('ALTER TABLE {{.prefix}}workspaces COLLATE ', @targetCollation);
 PREPARE stmt FROM @updateCollationQuery;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
