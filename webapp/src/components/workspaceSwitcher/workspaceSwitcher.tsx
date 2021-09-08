1059f0fda429ca1b5e109b5d53c818c5f4cd450c
Harshil Sharma <harshilsharma63@gmail.com>
2021-09-08 15:37:53 +0530

    Saving last visited workspace ID

--- webapp/src/components/workspaceSwitcher/workspaceSwitcher.tsx
+++ webapp/src/components/workspaceSwitcher/workspaceSwitcher.tsx
@@ -8,6 +8,8 @@ import {generatePath, useHistory, useRouteMatch} from 'react-router-dom'
 import {IWorkspace} from '../../blocks/workspace'
 import ChevronDown from '../../widgets/icons/chevronDown'
 
+import {UserSettings} from '../../userSettings'
+
 import WorkspaceOptions from './workspaceOptions'
 
 type Props = {
@@ -43,6 +45,7 @@ const WorkspaceSwitcher = (props: Props): JSX.Element => {
                     onChange={(workspaceId: string) => {
                         setShowMenu(false)
                         const newPath = generatePath(match.path, {workspaceId})
+                        UserSettings.lastWorkspaceId = workspaceId
                         history.replace(newPath)
                     }}
                 />
