5d4a7fc5affe454c94bd3a0ca3e2ad48576d6140
Scott Bishel <scott.bishel@mattermost.com>
2021-09-09 16:09:26 -0600

    remove log statement

--- webapp/src/components/propertyValueElement.tsx
+++ webapp/src/components/propertyValueElement.tsx
@@ -157,7 +157,6 @@ const PropertyValueElement = (props:Props): JSX.Element => {
                 </div>
             )
         }
-        Utils.log('ValueSelector')
         return (
             <ValueSelector
                 emptyValue={emptyDisplayValue}
