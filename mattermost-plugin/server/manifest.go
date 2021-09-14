f888938041c79a07a5f68845324bef999b71d482
Chen-I Lim <46905241+chenilim@users.noreply.github.com>
2021-09-14 09:53:52 -0700

    Fix #1203. Require v6.0.0 for plugin (#1204)

--- mattermost-plugin/server/manifest.go
+++ mattermost-plugin/server/manifest.go
@@ -20,7 +20,7 @@ const manifestStr = `
   "release_notes_url": "https://github.com/mattermost/focalboard/releases",
   "icon_path": "assets/starter-template-icon.svg",
   "version": "0.9.0",
-  "min_server_version": "5.37.0",
+  "min_server_version": "6.0.0",
   "server": {
     "executables": {
       "linux-amd64": "server/dist/plugin-linux-amd64",
