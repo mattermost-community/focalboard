73a44758eeabb80706d731c00f1a84544c9486f4
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2021-09-17 12:51:22 +0500

    Updating UI

--- webapp/src/pages/welcome/welcomePage.tsx
+++ webapp/src/pages/welcome/welcomePage.tsx
@@ -3,6 +3,8 @@
 import React from 'react'
 import {useHistory} from 'react-router-dom'
 
+import CompassIcon from '../../widgets/icons/compassIcon'
+
 import './welcomePage.scss'
 
 type Props = {
@@ -30,14 +32,28 @@ const WelcomePage = React.memo((props: Props) => {
 
     return (
         <div className='WelcomePage'>
-            <h1> {'Welcome To Boards'} </h1>
-            <h3> {'Boards is a project management tool that helps define, organize, track and manage work across teams, using a familiar kanban board view'} </h3>
+            <div>
+                <h1 className='text-heading6'> {'Welcome To Boards'} </h1>
+                <div className='text-base'> {'Boards is a project management tool that helps define, organize, track and manage work across teams, using a familiar kanban board view'} </div>
 
-            {/*Picture */}
+                <div className='WelcomePage__image'>
+                    <img
+                        src='../../../../boards-welcome.png'
+                        alt='Boards Welcome Image'
+                    />
+                </div>
 
-            <button onClick={goForward}>
-                {'Explore'}
-            </button>
+                <button
+                    onClick={goForward}
+                    className='Button filled size--large'
+                >
+                    {'Explore'}
+                    <CompassIcon
+                        icon='chevron-right'
+                        className='Icon Icon--right'
+                    />
+                </button>
+            </div>
         </div>
     )
 })
