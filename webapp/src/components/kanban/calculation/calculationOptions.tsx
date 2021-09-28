3a063adfacb8e053f007a25e65f3f4e3b80c00f3
Harshil Sharma <harshilsharma63@gmail.com>
2021-09-28 17:50:11 +0530

    Prepared submenu

--- webapp/src/components/kanban/calculation/calculationOptions.tsx
+++ webapp/src/components/kanban/calculation/calculationOptions.tsx
@@ -4,22 +4,29 @@ import React, {useState} from 'react'
 
 import {CalculationOptions, CalculationOptionsProps, optionsByType} from '../../calculations/options'
 import {IPropertyTemplate} from '../../../blocks/board'
+import './calculationOption.scss'
+import ChevronRight from '../../../widgets/icons/chevronRight'
 
 type Props = CalculationOptionsProps & {
     cardProperties: IPropertyTemplate[]
 }
 
-import './calculationOption.scss'
-
 const Option = (props: any): JSX.Element => {
     const [submenu, setSubmenu] = useState(false)
+    const [height, setHeight] = useState(0)
+    const [x, setX] = useState(0)
 
-    // const [height, setHeight] = useState(0)
-    const handleOption = (e: any) => {
+    const showOption = (e: any) => {
+        console.log('showOption')
+        console.log(e.target.getBoundingClientRect())
+        console.log(e.target.getBoundingClientRect().x + e.target)
+        console.log(e.target.getBoundingClientRect().y)
         if (submenu) {
             setSubmenu(false)
         } else {
-            // setHeight(e.clientY)
+            const rect = e.target.getBoundingClientRect()
+            setHeight(rect.y)
+            setX(rect.x + rect.width)
             setSubmenu(true)
         }
     }
@@ -32,15 +39,19 @@ const Option = (props: any): JSX.Element => {
     return (
         <div
             className='KanbanCalculationOptions_CustomOption'
-            onClick={handleOption}
+            onMouseEnter={showOption}
+            onMouseLeave={showOption}
         >
             <span>
-                {props.data.label}
+                {props.data.label} <ChevronRight/>
             </span>
 
             {
                 submenu && (
-                    <div className='dropdown-submenu'>
+                    <div
+                        className='dropdown-submenu'
+                        style={{top: `${height - 10}px`, left: `${x}px`}}
+                    >
                         <div
                             className='drops'
                             onClick={handleSubOption}
@@ -67,12 +78,10 @@ const Option = (props: any): JSX.Element => {
 }
 
 export const KanbanCalculationOptions = (props: Props): JSX.Element => {
-    const options = props.cardProperties.
-        map((property) => optionsByType.get(property.type) || []).
-        reduce((acc, propertyOptions) => {
-            acc.push(...propertyOptions)
-            return acc
-        })
+    const options = props.cardProperties.map((property) => optionsByType.get(property.type) || []).reduce((acc, propertyOptions) => {
+        acc.push(...propertyOptions)
+        return acc
+    })
 
     options.push(...optionsByType.get('common')!)
 
