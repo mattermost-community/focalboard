999a4e4f0d646c42e6fa5dec10e716c78a6008ee
Andrey Eremchenko <eremchenko@gmail.com>
2021-10-02 14:26:09 +0700

    Unit test for `CardDetailProperties` fixed:  - jest snapshot updated  - switched from mocking `fetch` to mocking `mutator`

--- webapp/src/components/cardDetail/cardDetailProperties.test.tsx
+++ webapp/src/components/cardDetail/cardDetailProperties.test.tsx
@@ -3,23 +3,17 @@
 import React from 'react'
 
 import {fireEvent, render} from '@testing-library/react'
-import {IntlProvider} from 'react-intl'
 import userEvent from '@testing-library/user-event'
+import {mocked} from 'ts-jest/utils'
 
+import {wrapIntl} from '../../testUtils'
 import {TestBlockFactory} from '../../test/testBlockFactory'
-import {FetchMock} from '../../test/fetchMock'
-
-import 'isomorphic-fetch'
+import mutator from '../../mutator'
 
 import CardDetailProperties from './cardDetailProperties'
 
-global.fetch = FetchMock.fn
-
-beforeEach(() => {
-    FetchMock.fn.mockReset()
-})
-
-const wrapIntl = (children: any) => <IntlProvider locale='en'>{children}</IntlProvider>
+jest.mock('../../mutator')
+const mockedMutator = mocked(mutator, true)
 
 describe('components/cardDetail/CardDetailProperties', () => {
     const board = TestBlockFactory.createBoard()
@@ -56,9 +50,6 @@ describe('components/cardDetail/CardDetailProperties', () => {
     const card = TestBlockFactory.createCard(board)
     card.fields.properties.property_id_1 = 'property_value_id_1'
 
-    const cardTemplate = TestBlockFactory.createCard(board)
-    cardTemplate.fields.isTemplate = true
-
     test('should match snapshot', async () => {
         const component = wrapIntl((
             <CardDetailProperties
@@ -78,11 +69,12 @@ describe('components/cardDetail/CardDetailProperties', () => {
     })
 
     test('rename select property', async () => {
+        const cards = [card]
         const component = wrapIntl((
             <CardDetailProperties
                 board={board!}
                 card={card}
-                cards={[card]}
+                cards={cards}
                 contents={[]}
                 comments={[]}
                 activeView={view}
@@ -96,22 +88,15 @@ describe('components/cardDetail/CardDetailProperties', () => {
         expect(propertyLabel).toBeDefined()
         fireEvent.click(propertyLabel!)
 
+        const newName = 'Owner - Renamed'
         const propertyNameInput = container.querySelector('.PropertyMenu.menu-textbox')
         expect(propertyNameInput).toBeDefined()
-        userEvent.type(propertyNameInput!, 'Owner - Renamed{enter}')
+        userEvent.type(propertyNameInput!, `${newName}{enter}`)
         fireEvent.click(propertyLabel!)
 
         // should be called once on renaming the property
-        expect(FetchMock.fn).toBeCalledTimes(1)
-
-        // Verify API call was made with renamed property
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const lastAPICallPayload = JSON.parse(FetchMock.fn.mock.calls[0][1].body)
-        expect(lastAPICallPayload[0].fields.cardProperties[0].name).toBe('Owner - Renamed')
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options.length).toBe(3)
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options[0].value).toBe('Jean-Luc Picard')
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options[1].value).toBe('William Riker')
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options[2].value).toBe('Deanna Troi')
+        const propertyTemplate = board.fields.cardProperties[0]
+        expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledTimes(1)
+        expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledWith(board, cards, propertyTemplate, 'select', newName)
     })
 })
