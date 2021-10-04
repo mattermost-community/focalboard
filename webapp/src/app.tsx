b62292fb3070336a34e61b54f97a8e1bf20023b3
Harshil Sharma <harshilsharma63@gmail.com>
2021-10-04 14:44:18 +0530

    Resolved issue regarding openign boards link from channels (#1388)
    * Resolved issue regarding openign boards link from channels
    
    * Removed unnecessary check

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -88,6 +88,7 @@ const App = React.memo((): JSX.Element => {
         dispatch(fetchLanguage())
         dispatch(fetchMe())
         dispatch(fetchClientConfig())
+        history.replace(window.location.pathname.replace((window as any).frontendBaseURL, ''))
     }, [])
 
     useEffect(() => {
@@ -109,8 +110,8 @@ const App = React.memo((): JSX.Element => {
         setTimeout(() => dispatch(setGlobalError('')), 0)
     }
 
-    const continueToWelcomeScreen = (boardIdIsValidUUIDV4 = true) => {
-        return Utils.isFocalboardPlugin() && loggedIn === true && (!UserSettings.welcomePageViewed || !boardIdIsValidUUIDV4)
+    const continueToWelcomeScreen = () => {
+        return Utils.isFocalboardPlugin() && loggedIn === true && !UserSettings.welcomePageViewed
     }
 
     const buildOriginalPath = (workspaceId = '', boardId = '', viewId = '', cardId = '') => {
@@ -234,7 +235,7 @@ const App = React.memo((): JSX.Element => {
                                             return <Redirect to='/login'/>
                                         }
 
-                                        if (continueToWelcomeScreen(boardIdIsValidUUIDV4)) {
+                                        if (continueToWelcomeScreen()) {
                                             const originalPath = `/${buildOriginalPath('', boardId, viewId, cardId)}`
                                             const queryString = boardIdIsValidUUIDV4 ? `r=${originalPath}` : ''
                                             return <Redirect to={`/welcome?${queryString}`}/>
