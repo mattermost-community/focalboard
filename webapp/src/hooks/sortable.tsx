922fe87e447fbe616cd1ef9a0245dd78e7224dc1
Tiago Silva <tiago.toya@hotmail.com>
2021-10-04 14:28:56 -0300

    refactor: simplifying typing using Generics

--- webapp/src/hooks/sortable.tsx
+++ webapp/src/hooks/sortable.tsx
@@ -5,18 +5,12 @@ import {useDrag, useDrop, DragElementWrapper, DragSourceOptions, DragPreviewOpti
 
 import {IContentBlockWithCords} from '../blocks/contentBlock'
 import {Block} from '../blocks/block'
-import {IPropertyOption, IPropertyTemplate} from '../blocks/board'
-import {Card} from '../blocks/card'
-
-type SortableItem = Card | IPropertyOption | IPropertyTemplate
-type HandlerItem = Card & IPropertyTemplate & IPropertyOption
-type HandlerBaseItem = HandlerItem & IContentBlockWithCords
 interface ISortableWithGripItem {
     block: Block | Block[],
     cords: {x: number, y?: number, z?: number}
 }
 
-function useSortableBase(itemType: string, item: SortableItem | ISortableWithGripItem, enabled: boolean, handler: (src: HandlerBaseItem, st: HandlerBaseItem) => void): [boolean, boolean, DragElementWrapper<DragSourceOptions>, DragElementWrapper<DragSourceOptions>, DragElementWrapper<DragPreviewOptions>] {
+function useSortableBase<T>(itemType: string, item: T, enabled: boolean, handler: (src: T, st: T) => void): [boolean, boolean, DragElementWrapper<DragSourceOptions>, DragElementWrapper<DragSourceOptions>, DragElementWrapper<DragPreviewOptions>] {
     const [{isDragging}, drag, preview] = useDrag(() => ({
         type: itemType,
         item,
@@ -30,8 +24,8 @@ function useSortableBase(itemType: string, item: SortableItem | ISortableWithGri
         collect: (monitor) => ({
             isOver: monitor.isOver(),
         }),
-        drop: (dragItem: HandlerBaseItem) => {
-            handler(dragItem, item as HandlerBaseItem)
+        drop: (dragItem: T) => {
+            handler(dragItem, item)
         },
         canDrop: () => enabled,
     }), [item, handler, enabled])
@@ -39,7 +33,7 @@ function useSortableBase(itemType: string, item: SortableItem | ISortableWithGri
     return [isDragging, isOver, drag, drop, preview]
 }
 
-export function useSortable(itemType: string, item: SortableItem, enabled: boolean, handler: (src: HandlerItem, st: HandlerItem) => void): [boolean, boolean, React.RefObject<HTMLDivElement>] {
+export function useSortable<T>(itemType: string, item: T, enabled: boolean, handler: (src: T, st: T) => void): [boolean, boolean, React.RefObject<HTMLDivElement>] {
     const ref = useRef<HTMLDivElement>(null)
     const [isDragging, isOver, drag, drop] = useSortableBase(itemType, item, enabled, handler)
     drop(drag(ref))
@@ -49,7 +43,7 @@ export function useSortable(itemType: string, item: SortableItem, enabled: boole
 export function useSortableWithGrip(itemType: string, item: ISortableWithGripItem, enabled: boolean, handler: (src: IContentBlockWithCords, st: IContentBlockWithCords) => void): [boolean, boolean, React.RefObject<HTMLDivElement>, React.RefObject<HTMLDivElement>] {
     const ref = useRef<HTMLDivElement>(null)
     const previewRef = useRef<HTMLDivElement>(null)
-    const [isDragging, isOver, drag, drop, preview] = useSortableBase(itemType, item, enabled, handler)
+    const [isDragging, isOver, drag, drop, preview] = useSortableBase(itemType, item as IContentBlockWithCords, enabled, handler)
     drag(ref)
     drop(preview(previewRef))
     return [isDragging, isOver, ref, previewRef]
