ef2122df558d0175e05c2eabe21a5715520084a3
Julien Fabre <julien.fabre.33@gmail.com>
2021-10-05 02:35:51 +0200

    chore: add test on is empty filterEntry

--- webapp/src/components/viewHeader/filterComponent.test.tsx
+++ webapp/src/components/viewHeader/filterComponent.test.tsx
@@ -10,6 +10,8 @@ import '@testing-library/jest-dom'
 
 import userEvent from '@testing-library/user-event'
 
+import {FilterClause} from '../../blocks/filterClause'
+
 import {TestBlockFactory} from '../../test/testBlockFactory'
 import mutator from '../../mutator'
 
@@ -19,22 +21,28 @@ import FilterComponenet from './filterComponent'
 
 jest.mock('../../mutator')
 const mockedMutator = mocked(mutator, true)
+const filter: FilterClause = {
+    propertyId: '1',
+    condition: 'includes',
+    values: ['Status'],
+}
 
 const board = TestBlockFactory.createBoard()
 const activeView = TestBlockFactory.createBoardView(board)
-
-describe('components/viewHeader/filterComponent', () => {
-    const state = {
-        users: {
-            me: {
-                id: 'user-id-1',
-                username: 'username_1',
-            },
+const state = {
+    users: {
+        me: {
+            id: 'user-id-1',
+            username: 'username_1',
         },
-    }
-    const store = mockStateStore([], state)
+    },
+}
+const store = mockStateStore([], state)
+describe('components/viewHeader/filterComponent', () => {
     beforeEach(() => {
         jest.clearAllMocks()
+        board.fields.cardProperties[0].options = [{id: 'Status', value: 'Status', color: ''}]
+        activeView.fields.filter.filters = [filter]
     })
     test('return filterComponent', () => {
         const {container} = render(
@@ -89,6 +97,26 @@ describe('components/viewHeader/filterComponent', () => {
         expect(container).toMatchSnapshot()
         const buttonStatus = screen.getByRole('button', {name: 'Status'})
         userEvent.click(buttonStatus)
+        expect(mockedMutator.changeViewFilter).toBeCalledTimes(1)
+    })
+
+    test('return filterComponent and click is empty', () => {
+        const {container} = render(
+            wrapIntl(
+                <ReduxProvider store={store}>
+                    <FilterComponenet
+                        board={board}
+                        activeView={activeView}
+                        onClose={jest.fn()}
+                    />
+                </ReduxProvider>,
+            ),
+        )
+        const buttonElement = screen.getAllByRole('button', {name: 'menuwrapper'})[1]
+        userEvent.click(buttonElement)
+        expect(container).toMatchSnapshot()
+        const buttonNotInclude = screen.getByRole('button', {name: 'is empty'})
+        userEvent.click(buttonNotInclude)
         expect(mockedMutator.changeViewFilter).toBeCalledTimes(1)
     })
 })
