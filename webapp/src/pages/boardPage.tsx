6abc976adb233fe63296f95c8f5c91dba754ebc3
Scott Bishel <scott.bishel@mattermost.com>
2021-10-06 15:50:00 -0600

    update comments

--- webapp/src/pages/boardPage.tsx
+++ webapp/src/pages/boardPage.tsx
@@ -110,6 +110,8 @@ const BoardPage = (props: Props): JSX.Element => {
         }
 
         Utils.log(`attachToBoard: ${boardId}`)
+
+        // Ensure boardViews is for our boardId before redirecting
         const isCorrectBoardView = boardViews.length > 0 && boardViews[0].parentId === boardId
         if (!viewId && (isCorrectBoardView)) {
             const newPath = generatePath(match.path, {...match.params, boardId, viewId: boardViews[0].id})
