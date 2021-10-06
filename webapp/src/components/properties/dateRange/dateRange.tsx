f69e9ae9275ca468af07f5b333358c950cf707d6
Alok Kumar Sahoo <45600289+aloks98@users.noreply.github.com>
2021-10-07 00:44:59 +0530

    GH-1439: do not show outside dates in datepicker (#1450)
    * GH-1439: add css to highlight adjacent month selected dates
    
    * GH-1439: do not show outside days in datepicker
    
    * GH-1439: remove log statement
    
    Co-authored-by: aloks98 <aloksahoo@acceldata.io>

--- webapp/src/components/properties/dateRange/dateRange.tsx
+++ webapp/src/components/properties/dateRange/dateRange.tsx
@@ -156,7 +156,6 @@ function DateRange(props: Props): JSX.Element {
     if (!buttonText && showEmptyPlaceholder) {
         buttonText = intl.formatMessage({id: 'DateRange.empty', defaultMessage: 'Empty'})
     }
-
     return (
         <div className={`DateRange ${displayValue ? '' : 'empty'} ` + className}>
             <Button
@@ -235,7 +234,7 @@ function DateRange(props: Props): JSX.Element {
                             <DayPicker
                                 onDayClick={handleDayClick}
                                 initialMonth={dateFrom || new Date()}
-                                showOutsideDays={true}
+                                showOutsideDays={false}
                                 locale={locale}
                                 localeUtils={MomentLocaleUtils}
                                 todayButton={intl.formatMessage({id: 'DateRange.today', defaultMessage: 'Today'})}
