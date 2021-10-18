103607a432d2b8fb35e3e300e41bd4fab662aacb
Mateusz Sip <mateusz.sip@gmail.com>
2021-10-18 12:21:56 +0200

    Fix imports

--- server/integrationtests/workspaces_test.go
+++ server/integrationtests/workspaces_test.go
@@ -1,9 +1,10 @@
 package integrationtests
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/mattermost/focalboard/server/utils"
 
