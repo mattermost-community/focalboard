e1b953b080cbb9c923e8326afc8ba8e6f359a69c
Andrey Eremchenko <eremchenko@gmail.com>
2021-10-22 21:27:37 +0700

    Width of button `+ Add a property` is fixed.

--- webapp/src/components/cardDetail/cardDetailProperties.tsx
+++ webapp/src/components/cardDetail/cardDetailProperties.tsx
@@ -107,31 +107,31 @@ const CardDetailProperties = React.memo((props: Props) => {
             )}
 
             {!props.readonly &&
-                <MenuWrapper>
-                    <div className='octo-propertyname add-property'>
+                <div className='octo-propertyname add-property'>
+                    <MenuWrapper>
                         <Button>
                             <FormattedMessage
                                 id='CardDetail.add-property'
                                 defaultMessage='+ Add a property'
                             />
                         </Button>
-                    </div>
-                    <Menu>
-                        <PropertyTypes
-                            label={intl.formatMessage({id: 'PropertyMenu.selectType', defaultMessage: 'Select property type'})}
-                            onTypeSelected={async (type) => {
-                                const template: IPropertyTemplate = {
-                                    id: Utils.createGuid(IDType.BlockID),
-                                    name: typeDisplayName(intl, type),
-                                    type,
-                                    options: [],
-                                }
-                                const templateId = await mutator.insertPropertyTemplate(board, activeView, -1, template)
-                                setNewTemplateId(templateId)
-                            }}
-                        />
-                    </Menu>
-                </MenuWrapper>
+                        <Menu>
+                            <PropertyTypes
+                                label={intl.formatMessage({id: 'PropertyMenu.selectType', defaultMessage: 'Select property type'})}
+                                onTypeSelected={async (type) => {
+                                    const template: IPropertyTemplate = {
+                                        id: Utils.createGuid(IDType.BlockID),
+                                        name: typeDisplayName(intl, type),
+                                        type,
+                                        options: [],
+                                    }
+                                    const templateId = await mutator.insertPropertyTemplate(board, activeView, -1, template)
+                                    setNewTemplateId(templateId)
+                                }}
+                            />
+                        </Menu>
+                    </MenuWrapper>
+                </div>
             }
         </div>
     )
