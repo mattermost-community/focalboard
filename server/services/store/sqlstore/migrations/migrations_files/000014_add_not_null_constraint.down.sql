2d9705baa9078e60a9ef2b3d7bc459af776dc964
Harshil Sharma <harshilsharma63@gmail.com>
2021-10-22 10:15:00 +0530

    Fixed incorrect column name in migration

--- server/services/store/sqlstore/migrations/migrations_files/000014_add_not_null_constraint.down.sql
+++ server/services/store/sqlstore/migrations/migrations_files/000014_add_not_null_constraint.down.sql
@@ -1,9 +1,9 @@
 {{if .mysql}}
-ALTER TABLE {{.prefix}}blocks MODIFY creatde_by varchar(36);
-ALTER TABLE {{.prefix}}blocks MODIFY updated_by varchar(36);
+ALTER TABLE {{.prefix}}blocks MODIFY created_by varchar(36);
+ALTER TABLE {{.prefix}}blocks MODIFY modified_by varchar(36);
 {{end}}
 
 {{if .postgres}}
 ALTER TABLE {{.prefix}}blocks ALTER COLUMN created_by DROP NOT NULL;
-ALTER TABLE {{.prefix}}blocks ALTER COLUMN updated_by DROP NOT NULL;
+ALTER TABLE {{.prefix}}blocks ALTER COLUMN modified_by DROP NOT NULL;
 {{end}}
