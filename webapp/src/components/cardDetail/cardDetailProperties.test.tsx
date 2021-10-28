4bdfcd365642e242cf88d41961730dff76ce1358
Prakhar <>
2021-10-28 23:42:38 +0530

    imported library for failing test

--- webapp/src/components/cardDetail/cardDetailProperties.test.tsx
+++ webapp/src/components/cardDetail/cardDetailProperties.test.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {mocked} from 'ts-jest/utils'
-
+import '@testing-library/jest-dom'
 import {createIntl} from 'react-intl'
 
 import {PropertyType} from '../../blocks/board'
