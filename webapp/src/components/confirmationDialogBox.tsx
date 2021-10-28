253c054f8470cddc0ee1d5bb7f275dc714b491fa
Prakhar <>
2021-10-28 14:16:03 +0530

    Added memo and useCallback for cnfrm dialog component

--- webapp/src/components/confirmationDialogBox.tsx
+++ webapp/src/components/confirmationDialogBox.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react'
+import React, {useCallback} from 'react'
 import {FormattedMessage} from 'react-intl'
 
 import Button from '../widgets/buttons/button'
@@ -22,10 +22,13 @@ type Props = {
 }
 
 export const ConfirmationDialogBox = (props: Props) => {
+    const handleOnClose = useCallback(props.dialogBox.onClose,[])
+    const handleOnConfirm = useCallback(props.dialogBox.onConfirm,[])
+
     return (
         <Dialog
             className='confirmation-dialog-box'
-            onClose={props.dialogBox.onClose}
+            onClose={handleOnClose}
         >
             <div className='box-area'>
                 <h3 className='heading'>{props.dialogBox.heading}</h3>
@@ -35,7 +38,7 @@ export const ConfirmationDialogBox = (props: Props) => {
                     <Button
                         title='Cancel'
                         active={true}
-                        onClick={props.dialogBox.onClose}
+                        onClick={handleOnClose}
                     >
                         <FormattedMessage
                             id='ConfirmationDialog.cancel-action'
@@ -46,7 +49,7 @@ export const ConfirmationDialogBox = (props: Props) => {
                         title={props.dialogBox.confirmButtonText || 'Confirm'}
                         submit={true}
                         emphasis='danger'
-                        onClick={props.dialogBox.onConfirm}
+                        onClick={handleOnConfirm}
                     >
                         { props.dialogBox.confirmButtonText ||
                         <FormattedMessage
