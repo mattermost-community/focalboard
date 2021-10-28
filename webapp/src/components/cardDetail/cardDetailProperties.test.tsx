9b387c8ddf537ba7e78a41c2dcf6a6c0e5a1335a
Prakhar <>
2021-10-28 22:44:47 +0530

    acknowledged @kamre comments

--- webapp/src/components/cardDetail/cardDetailProperties.test.tsx
+++ webapp/src/components/cardDetail/cardDetailProperties.test.tsx
@@ -5,15 +5,14 @@
 // import 'isomorphic-fetch'
 
 import React from 'react'
-import {render, screen, act, fireEvent} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import {mocked} from 'ts-jest/utils'
 
 import {createIntl} from 'react-intl'
 
-import {IPropertyTemplate, PropertyType} from '../../blocks/board'
-import {FetchMock} from '../../test/fetchMock'
+import {PropertyType} from '../../blocks/board'
 import {wrapIntl} from '../../testUtils'
 import {TestBlockFactory} from '../../test/testBlockFactory'
 import mutator from '../../mutator'
@@ -65,7 +64,7 @@ describe('components/cardDetail/CardDetailProperties', () => {
 
     const card = TestBlockFactory.createCard(board)
     card.fields.properties.property_id_1 = 'property_value_id_1'
-    card.fields.properties.property_id_2 = 'property_value_id_2'
+    card.fields.properties.property_id_2 = '1234' //  random number
 
     const cardTemplate = TestBlockFactory.createCard(board)
     cardTemplate.fields.isTemplate = true
@@ -86,8 +85,7 @@ describe('components/cardDetail/CardDetailProperties', () => {
             />
         ))
 
-        const result = render(component)
-        return result
+        return render(component)
     }
 
     it('should match snapshot', async () => {
@@ -95,25 +93,25 @@ describe('components/cardDetail/CardDetailProperties', () => {
         expect(container).toMatchSnapshot()
     })
 
-    it('should rename existing select property', async () => {
-        const result = renderComponent()
+    // it('should rename existing select property', async () => {
+    //     const result = renderComponent()
 
-        const menuElement = screen.getByRole('button', {name: 'Owner'})
-        userEvent.click(menuElement)
+    //     const menuElement = screen.getByRole('button', {name: 'Owner'})
+    //     userEvent.click(menuElement)
 
-        const newName = 'Owner - Renamed'
-        const propertyNameInput = screen.getAllByRole('textbox')
-        userEvent.type(propertyNameInput[0], `${newName}{enter}`)
+    //     const newName = 'Owner - Renamed'
+    //     const propertyNameInput = screen.getByRole('textbox')
+    //     userEvent.type(propertyNameInput, `${newName}{enter}`)
 
-        const propertyTemplate = board.fields.cardProperties[0]
+    //     const propertyTemplate = board.fields.cardProperties[0]
 
-        const confirmButton = result.getByTitle('Change Property')
-        expect(confirmButton).toBeDefined()
+    //     const confirmButton = result.getByTitle('Change Property')
+    //     expect(confirmButton).toBeDefined()
 
-        userEvent.click(confirmButton!)
-        expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledTimes(1)
-        expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledWith(board, cards, propertyTemplate, 'select', newName)
-    })
+    //     userEvent.click(confirmButton!)
+    //     expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledTimes(1)
+    //     expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledWith(board, cards, propertyTemplate, 'select', newName)
+    // })
 
     it('should show confirmation dialog when deleting existing select property', () => {
         renderComponent()
@@ -145,10 +143,13 @@ describe('components/cardDetail/CardDetailProperties', () => {
         })
     })
 
-    test('rename select property and confirm on dialog should rename property', async () => {
+    test('rename select property and confirm button on dialog should rename property', async () => {
         const result = renderComponent()
 
+        // rename to "Owner-Renamed"
         onPropertyRenameOpenConfirmationDialog(result.container)
+
+        const propertyTemplate = board.fields.cardProperties[0]
 
         const confirmButton = result.getByTitle('Change Property')
         expect(confirmButton).toBeDefined()
@@ -156,21 +157,12 @@ describe('components/cardDetail/CardDetailProperties', () => {
         userEvent.click(confirmButton!)
 
         // should be called once on confirming renaming the property
-        expect(FetchMock.fn).toBeCalledTimes(1)
-
-        // Verify API call was made with renamed property
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const lastAPICallPayload = JSON.parse(FetchMock.fn.mock.calls[0][1].body)
-        expect(lastAPICallPayload[0].fields.cardProperties[0].name).toBe('Owner - Renamed')
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options.length).toBe(3)
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options[0].value).toBe('Jean-Luc Picard')
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options[1].value).toBe('William Riker')
-        expect(lastAPICallPayload[0].fields.cardProperties[0].options[2].value).toBe('Deanna Troi')
+        expect(mockedMutator.changePropertyTypeAndName).toBeCalledTimes(1)
+        expect(mockedMutator.changePropertyTypeAndName).toHaveBeenCalledWith(board, cards, propertyTemplate, 'select', 'Owner - Renamed')
     })
 
     it('should add new number property', async () => {
-        const result = renderComponent()
+        renderComponent()
 
         const menuElement = screen.getByRole('button', {name: /add a property/i})
         userEvent.click(menuElement)
@@ -179,11 +171,6 @@ describe('components/cardDetail/CardDetailProperties', () => {
             const numberType = screen.getByRole('button', {name: /number/i})
             userEvent.click(numberType)
         })
-
-        const confirmButton = result.getByTitle('Change Property')
-        expect(confirmButton).toBeDefined()
-
-        userEvent.click(confirmButton!)
 
         expect(mockedMutator.insertPropertyTemplate).toHaveBeenCalledTimes(1)
 
@@ -194,7 +181,7 @@ describe('components/cardDetail/CardDetailProperties', () => {
         expect(template!.type).toBe('number')
     })
 
-    test('cancel btn in TypeorNameChange dialog should do nothing', () => {
+    it('cancel button in TypeorNameChange dialog should do nothing', () => {
         const result = renderComponent()
         const container = result.container
         onPropertyRenameOpenConfirmationDialog(container)
@@ -207,11 +194,13 @@ describe('components/cardDetail/CardDetailProperties', () => {
         expect(container).toMatchSnapshot()
     })
 
-    test('confirmation on delete dialog should delete the property', () => {
+    it('confirmation on delete dialog should delete the property', () => {
         const result = renderComponent()
         const container = result.container
 
         openDeleteConfirmationDialog(container)
+
+        const propertyTemplate = board.fields.cardProperties[0]
 
         const confirmButton = result.getByTitle('Delete')
         expect(confirmButton).toBeDefined()
@@ -220,19 +209,11 @@ describe('components/cardDetail/CardDetailProperties', () => {
         userEvent.click(confirmButton!)
 
         // should be called once on confirming delete
-        expect(FetchMock.fn).toBeCalledTimes(1)
-
-        // Verify API call was made and deleted property does not existing
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const lastAPICallPayload = JSON.parse(FetchMock.fn.mock.calls[0][1].body)
-        const deletedPropInPayload = lastAPICallPayload[0].fields.cardProperties.filter(
-            (prop:IPropertyTemplate) => prop.id === board.fields.cardProperties[0].id)
-
-        expect(deletedPropInPayload.length).toBe(0)
+        expect(mockedMutator.deleteProperty).toBeCalledTimes(1)
+        expect(mockedMutator.deleteProperty).toBeCalledWith(board, views, cards, propertyTemplate.id)
     })
 
-    test('cancel on delete dialog should do nothing', () => {
+    it('cancel on delete dialog should do nothing', () => {
         const result = renderComponent()
         const container = result.container
 
@@ -248,7 +229,7 @@ describe('components/cardDetail/CardDetailProperties', () => {
     function openDeleteConfirmationDialog(container:HTMLElement) {
         const propertyLabel = container.querySelector('.MenuWrapper')
         expect(propertyLabel).toBeDefined()
-        fireEvent.click(propertyLabel!)
+        userEvent.click(propertyLabel!)
 
         const deleteOption = container.querySelector('.MenuOption.TextOption')
         expect(propertyLabel).toBeDefined()
@@ -261,13 +242,13 @@ describe('components/cardDetail/CardDetailProperties', () => {
     function onPropertyRenameOpenConfirmationDialog(container:HTMLElement) {
         const propertyLabel = container.querySelector('.MenuWrapper')
         expect(propertyLabel).toBeDefined()
-        fireEvent.click(propertyLabel!)
+        userEvent.click(propertyLabel!)
 
         // write new name in the name text box
         const propertyNameInput = container.querySelector('.PropertyMenu.menu-textbox')
         expect(propertyNameInput).toBeDefined()
         userEvent.type(propertyNameInput!, 'Owner - Renamed{enter}')
-        fireEvent.click(propertyLabel!)
+        userEvent.click(propertyLabel!)
 
         const confirmDialog = container.querySelector('.dialog.confirmation-dialog-box')
         expect(confirmDialog).toBeDefined()
