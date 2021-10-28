3a61a01033129d3aa5ae77091ba2f3daae0593d9
Prakhar <>
2021-10-28 23:25:05 +0530

    removed unwanted comments

--- webapp/src/components/cardDetail/cardDetailProperties.test.tsx
+++ webapp/src/components/cardDetail/cardDetailProperties.test.tsx
@@ -1,8 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-// import {fireEvent, render} from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
-// import 'isomorphic-fetch'
 
 import React from 'react'
 import {render, screen, act} from '@testing-library/react'
