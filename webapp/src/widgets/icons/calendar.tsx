e16e715e8a7687d81d538e3473207453191be4a4
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2021-11-10 15:27:24 +0500

    Updating icons for calendar mvp

--- webapp/src/widgets/icons/calendar.tsx
+++ webapp/src/widgets/icons/calendar.tsx
@@ -11,74 +11,16 @@ export default function CalendarIcon(): JSX.Element {
             width='24'
             height='24'
             viewBox='0 0 24 24'
-            fill='none'
-            className='CalendarIcon Icon'
+            fill='currentColor'
             xmlns='http://www.w3.org/2000/svg'
+            className='CalendarIcon Icon'
         >
-            <g opacity='0.64'>
-                <rect
-                    x='3'
-                    y='3'
-                    width='18'
-                    height='18'
-                    rx='1'
-                    stroke='white'
-                    strokeWidth='2'
-                />
-                <rect
-                    x='20'
-                    y='7'
-                    width='2'
-                    height='16'
-                    transform='rotate(90 20 7)'
-                    fill='white'
-                />
-                <rect
-                    x='6'
-                    y='11'
-                    width='2'
-                    height='2'
-                    fill='white'
-                />
-                <rect
-                    x='8'
-                    y='15'
-                    width='2'
-                    height='2'
-                    transform='rotate(90 8 15)'
-                    fill='white'
-                />
-                <rect
-                    x='13'
-                    y='11'
-                    width='2'
-                    height='2'
-                    transform='rotate(90 13 11)'
-                    fill='white'
-                />
-                <rect
-                    x='13'
-                    y='15'
-                    width='2'
-                    height='2'
-                    transform='rotate(90 13 15)'
-                    fill='white'
-                />
-                <rect
-                    x='18'
-                    y='11'
-                    width='2'
-                    height='2'
-                    transform='rotate(90 18 11)'
-                    fill='white'
-                />
-                <rect
-                    x='18'
-                    y='15'
-                    width='2'
-                    height='2'
-                    transform='rotate(90 18 15)'
-                    fill='white'
+            <g opacity='0.8'>
+                <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M4 4H20V7L4 7V4ZM4 9L4 20H20V9L4 9ZM2 4C2 2.89543 2.89543 2 4 2H20C21.1046 2 22 2.89543 22 4V20C22 21.1046 21.1046 22 20 22H4C2.89543 22 2 21.1046 2 20V4ZM6 11H8V13H6V11ZM8 17V15H6V17H8ZM13 11V13H11V11H13ZM13 17V15H11V17H13ZM18 11V13H16V11H18ZM18 17V15H16V17H18Z'
+                    fill='currentColor'
                 />
             </g>
         </svg>
