7c15d72c76b6fe1086a89fd22e708bd26f066350
Prakhar <>
2021-11-10 17:34:39 +0530

    removed use of useCallback hook in for confirmationDialogBox

--- webapp/src/components/confirmationDialogBox.tsx
+++ webapp/src/components/confirmationDialogBox.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useCallback} from 'react'
+import React from 'react'
 import {FormattedMessage} from 'react-intl'
 
 import Button from '../widgets/buttons/button'
@@ -22,13 +22,10 @@ type Props = {
 }
 
 export const ConfirmationDialogBox = (props: Props) => {
-    const handleOnClose = useCallback(props.dialogBox.onClose, [])
-    const handleOnConfirm = useCallback(props.dialogBox.onConfirm, [])
-
     return (
         <Dialog
             className='confirmation-dialog-box'
-            onClose={handleOnClose}
+            onClose={props.dialogBox.onClose}
         >
             <div className='box-area'>
                 <h3 className='heading'>{props.dialogBox.heading}</h3>
@@ -38,7 +35,7 @@ export const ConfirmationDialogBox = (props: Props) => {
                     <Button
                         title='Cancel'
                         active={true}
-                        onClick={handleOnClose}
+                        onClick={props.dialogBox.onClose}
                     >
                         <FormattedMessage
                             id='ConfirmationDialog.cancel-action'
@@ -49,7 +46,7 @@ export const ConfirmationDialogBox = (props: Props) => {
                         title={props.dialogBox.confirmButtonText || 'Confirm'}
                         submit={true}
                         emphasis='danger'
-                        onClick={handleOnConfirm}
+                        onClick={props.dialogBox.onConfirm}
                     >
                         { props.dialogBox.confirmButtonText ||
                         <FormattedMessage
