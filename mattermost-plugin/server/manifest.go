feb32b123f5cfdca19a8eb2408cc5e2aeba452a8
Carlos Tadeu Panato Junior <carlos@mattermost.com>
2021-11-26 09:44:26 +0100

    dist: build linux/darwin arm64 targets (#1854)

--- mattermost-plugin/server/manifest.go
+++ mattermost-plugin/server/manifest.go
@@ -25,7 +25,9 @@ const manifestStr = `
   "server": {
     "executables": {
       "darwin-amd64": "server/dist/plugin-darwin-amd64",
+      "darwin-arm64": "server/dist/plugin-darwin-arm64",
       "linux-amd64": "server/dist/plugin-linux-amd64",
+      "linux-arm64": "server/dist/plugin-linux-arm64",
       "windows-amd64": "server/dist/plugin-windows-amd64.exe"
     },
     "executable": ""
