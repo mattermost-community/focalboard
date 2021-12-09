c39311424ce27f1d86c7d4daac9a3a7a5919877e
Chen-I Lim <chenilim@gmail.com>
2021-12-09 11:24:53 -0800

    Fix types

--- webapp/src/components/live-markdown-plugin/pluginStrategy.ts
+++ webapp/src/components/live-markdown-plugin/pluginStrategy.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import * as React from 'react'
 import {ContentBlock, ContentState} from 'draft-js'
 
 export interface InlineStrategy {
