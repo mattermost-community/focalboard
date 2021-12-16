bc2707d461b705c21035f87387d5db73d4b06f5f
Miguel de la Cruz <miguel@mcrx.me>
2021-12-16 11:25:06 +0100

    Add workspace to teams and boards migrations (#1986)
    * Add workspace to teams and boards migrations
    
    * Update json annotations on board models

---
+++ server/model/team.go
@@ -0,0 +1,45 @@
+package model
+
+// Team is information global to a team
+// swagger:model
+type Team struct {
+	// ID of the team
+	// required: true
+	ID string `json:"id"`
+
+	// Title of the team
+	// required: false
+	Title string `json:"title"`
+
+	// Token required to register new users
+	// required: true
+	SignupToken string `json:"signupToken"`
+
+	// Team settings
+	// required: false
+	Settings map[string]interface{} `json:"settings"`
+
+	// ID of user who last modified this
+	// required: true
+	ModifiedBy string `json:"modifiedBy"`
+
+	// Updated time
+	// required: true
+	UpdateAt int64 `json:"updateAt"`
+}
+
+// UserTeam is a summary of a single association between
+// a user and a team
+// swagger:model
+type UserTeam struct {
+	// ID of the team
+	// required: true
+	ID string `json:"id"`
+
+	// Title of the team
+	// required: false
+	Title string `json:"title"`
+
+	// Number of boards in the team
+	BoardCount int `json:"boardCount"`
+}
