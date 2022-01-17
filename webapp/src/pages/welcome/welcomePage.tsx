2d8c9a7a8a6610200fa4f5c4a6fb21e3ca56f6a6
Jesús Espino <jespinog@gmail.com>
2022-01-17 20:21:25 +0100

    Moving the history instance creation after the plugin initialization (to honor the SiteURL config) (#2109)
    * Moving the history instance creation after the plugin initialization (to honor the SiteURL config)
    
    * Fixing welcome page images urls generation
    
    * Fixing share board url generation
    
    * Fixing more subpath problems
    
    * Adding some tests with subpath
    
    * Adding subpath test to welcome page
    
    * fix linter error

--- webapp/src/pages/welcome/welcomePage.tsx
+++ webapp/src/pages/welcome/welcomePage.tsx
@@ -11,6 +11,7 @@ import BoardWelcomeSmallPNG from '../../../static/boards-welcome-small.png'
 import Button from '../../widgets/buttons/button'
 import CompassIcon from '../../widgets/icons/compassIcon'
 import {UserSettings} from '../../userSettings'
+import {Utils} from '../../utils'
 
 import './welcomePage.scss'
 
@@ -52,14 +53,14 @@ const WelcomePage = React.memo(() => {
 
                 {/* This image will be rendered on large screens over 2000px */}
                 <img
-                    src={BoardWelcomePNG}
+                    src={Utils.buildURL(BoardWelcomePNG, true)}
                     className='WelcomePage__image WelcomePage__image--large'
                     alt='Boards Welcome Image'
                 />
 
                 {/* This image will be rendered on small screens below 2000px */}
                 <img
-                    src={BoardWelcomeSmallPNG}
+                    src={Utils.buildURL(BoardWelcomeSmallPNG, true)}
                     className='WelcomePage__image WelcomePage__image--small'
                     alt='Boards Welcome Image'
                 />
