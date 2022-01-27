750c2822d17a985a1de38afb693143639311c4a5
wiggin77 <dlauder@warpmail.net>
2022-01-27 18:40:35 -0500

    import wip - zipstream test

--- server/app/import.go
+++ server/app/import.go
@@ -2,11 +2,19 @@ package app
 
 import (
 	"io"
+
+	"github.com/krolaw/zipstream"
 )
 
 const (
 	archiveVersion = 2
 )
 
+// ImportArchive imports an archive containing zero or more boards, plus all
+// associated content, including cards, content blocks, views, and images.
+//
+// Archives are ZIP files containing a `version.json` file and zero or more
+// directories, each containing a `board.jsonl` and zero or more image files.
 func (a *App) ImportArchive(r io.Reader) error {
+	zr, err := zipstream.NewReader()
 }
