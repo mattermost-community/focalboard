c18ac5c48876c48d7f901bde13508c78f8a8ff9f
Scott Bishel <scott.bishel@mattermost.com>
2022-01-28 08:53:56 -0700

    upgrade marked to 4.0.12

--- webapp/src/utils.ts
+++ webapp/src/utils.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import marked from 'marked'
+import {marked} from 'marked'
 import {IntlShape} from 'react-intl'
 import moment from 'moment'
 
