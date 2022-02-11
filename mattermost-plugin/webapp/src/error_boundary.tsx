3b3cbd17ffa071cc7e597c8091a0087a47761553
wiggin77 <wiggin77@warpmail.net>
2022-02-11 01:43:28 -0500

    clean compile

--- mattermost-plugin/webapp/src/error_boundary.tsx
+++ mattermost-plugin/webapp/src/error_boundary.tsx
@@ -2,28 +2,39 @@
 // See LICENSE.txt for license information.
 
 import React from 'react'
-import {Redirect} from 'react-router-dom'
+import {useHistory} from 'react-router'
+import PropTypes from 'prop-types'
 
-type Props = {}
 type State = {
     hasError: boolean
 }
 
-export default class ErrorBoundary<Props, State> extends React.Component {
+export default class ErrorBoundary extends React.Component {
     state = {hasError: false}
+    history = useHistory()
+    propTypes = {children: PropTypes.node.isRequired}
+    msg = 'Redirecting to error page...'
 
-    static getDerivedStateFromError(error: Error) {
+    static getDerivedStateFromError(/*error: Error*/): State {
         return {hasError: true}
     }
 
-    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        /* eslint-disable no-console */
         console.log(error, errorInfo)
+        /* eslint-enable no-console */
     }
 
-    render() {
+    shouldComponentUpdate(): boolean {
+        return true
+    }
+
+    render(): React.ReactNode {
         if (this.state.hasError) {
-            return <Redirect to='/error?id=unknown'/>
+            this.history.push('/error?id=unknown')
+            return <span>{this.msg}</span>
         }
         return this.props.children
     }
 }
+
