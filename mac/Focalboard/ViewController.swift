4ff979cf3acf8280aae89c375b3de4f1b3d85dc0
Chen-I Lim <chenilim@gmail.com>
2022-02-14 14:02:41 -0800

    Fix #2333. Handle export in Mac App

--- mac/Focalboard/ViewController.swift
+++ mac/Focalboard/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController:
 	@IBOutlet var webView: CustomWKWebView!
 	private var didLoad = false
 	private var refreshWebViewOnLoad = true
+	private let downloadHandler = DownloadHandler()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -100,60 +101,6 @@ class ViewController:
 		webView.load(request)
 	}
 
-	private func downloadJsonUrl(_ url: URL) {
-		NSLog("downloadJsonUrl")
-		let prefix = "data:text/json,"
-		let urlString = url.absoluteString
-		let encodedJson = String(urlString[urlString.index(urlString.startIndex, offsetBy: prefix.lengthOfBytes(using: .utf8))...])
-		guard let json = encodedJson.removingPercentEncoding else {
-			return
-		}
-
-		// Form file name
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "yyyy-M-d"
-		let dateString = dateFormatter.string(from: Date())
-		let filename = "archive-\(dateString).focalboard"
-
-		// Save file
-		let savePanel = NSSavePanel()
-		savePanel.canCreateDirectories = true
-		savePanel.nameFieldStringValue = filename
-		// BUGBUG: Specifying the allowedFileTypes causes Catalina to hang / error out
-		//savePanel.allowedFileTypes = [".focalboard"]
-		savePanel.begin { (result) in
-			if result.rawValue == NSApplication.ModalResponse.OK.rawValue,
-			   let fileUrl = savePanel.url {
-				try? json.write(to: fileUrl, atomically: true, encoding: .utf8)
-			}
-		}
-	}
-
-	private func downloadCsvUrl(_ url: URL) {
-		NSLog("downloadCsvUrl")
-		let prefix = "data:text/csv;charset=utf-8,"
-		let urlString = url.absoluteString
-		let encodedContents = String(urlString[urlString.index(urlString.startIndex, offsetBy: prefix.lengthOfBytes(using: .utf8))...])
-		guard let contents = encodedContents.removingPercentEncoding else {
-			return
-		}
-
-		let filename = "data.csv"
-
-		// Save file
-		let savePanel = NSSavePanel()
-		savePanel.canCreateDirectories = true
-		savePanel.nameFieldStringValue = filename
-		// BUGBUG: Specifying the allowedFileTypes causes Catalina to hang / error out
-		//savePanel.allowedFileTypes = [".focalboard"]
-		savePanel.begin { (result) in
-			if result.rawValue == NSApplication.ModalResponse.OK.rawValue,
-			   let fileUrl = savePanel.url {
-				try? contents.write(to: fileUrl, atomically: true, encoding: .utf8)
-			}
-		}
-	}
-
 	func webView(_ webView: WKWebView, runOpenPanelWith parameters: WKOpenPanelParameters, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping ([URL]?) -> Void) {
 		NSLog("webView runOpenPanel")
 		let openPanel = NSOpenPanel()
@@ -169,22 +116,30 @@ class ViewController:
 		}
 	}
 
-	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-		if let url = navigationAction.request.url {
-			// Intercept archive downloads, and present native UI
-			if (url.absoluteString.hasPrefix("data:text/json,")) {
-				decisionHandler(.cancel)
-				downloadJsonUrl(url)
-				return
-			}
-			if (url.absoluteString.hasPrefix("data:text/csv;charset=utf-8,")) {
-				decisionHandler(.cancel)
-				downloadCsvUrl(url)
-				return
-			}
-			NSLog("decidePolicyFor navigationAction: \(url.absoluteString)")
+	// Handle downloads
+	
+	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+		if navigationAction.shouldPerformDownload {
+			decisionHandler(.download, preferences)
+		} else {
+			decisionHandler(.allow, preferences)
 		}
-		decisionHandler(.allow)
+	}
+
+	func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+		if navigationResponse.canShowMIMEType {
+			decisionHandler(.allow)
+		} else {
+			decisionHandler(.download)
+		}
+	}
+
+	func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
+		download.delegate = downloadHandler
+	}
+
+	func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+		download.delegate = downloadHandler
 	}
 
 	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
