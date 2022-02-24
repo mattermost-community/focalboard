292a1b4b5eb8987e841263621177d8992cae7b67
Harshil Sharma <harshilsharma63@gmail.com>
2022-02-24 19:13:38 +0530

    WIP

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,14 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:8000/bjgjcpx9p3jgu3p9ksjbihwhgca/vsn1ccwu4b3djbyiq5hhnai4qjc'],
-            startServerCommand: './bin/focalboard-server',
+            url: [
+                'http://localhost:8000/byumge79udt8ut8x3ok9jo7xggo/vz1jgngxfwifftcka86upt11fyw', // all tasks view
+                'http://localhost:8000/byumge79udt8ut8x3ok9jo7xggo/vrh4d1mg4dpy65guz8b87m4zh5w', // by priority - kanban view
+                'http://localhost:8000/byumge79udt8ut8x3ok9jo7xggo/vpqgzdxh96tgq9eht55joqgd9ta', // by status
+                'http://localhost:8000/byumge79udt8ut8x3ok9jo7xggo/vw3og338nqidhxn9mkprhmauroo', // gallery view
+                'http://localhost:8000/byumge79udt8ut8x3ok9jo7xggo/vksq3tsg9xpb3fkjdcj9gngzz7h', // calender view
+            ],
+            startServerCommand: './bin/focalboard-server -dbconfig=./focalboard-lighthouse.db > ./bin/focalboard.logs',
             puppeteerScript: "./puppeteer/login.js",
             settings: {
                 // Don't clear localStorage/IndexedDB/etc before loading the page.
--- puppeteer/login.js
+++ puppeteer/login.js
@@ -1,3 +1,7 @@
+const EMAIL = 'focalboard@mattermost.com';
+const USERNAME = 'focalboard_user';
+const PASSWORD = 'focalboard_password';
+
 /**
  * @param {puppeteer.Browser} browser
  * @param {{url: string, options: LHCI.CollectCommand.Options}} context
@@ -5,11 +9,36 @@
 module.exports = async (browser, context) => {
     // launch browser for LHCI
     const page = await browser.newPage();
-    await page.goto('http://localhost:8000/login');
-    await page.type('#login-username', 'harshil.sharma');
-    await page.type('#login-password', 'mattermost');
-    await page.click('[type="submit"]');
-    await page.waitForNavigation();
+    await login(page)
+
     // close session for next run
     await page.close();
 };
+
+async function login(page) {
+    await page.goto('http://localhost:8000/login');
+    await page.type('#login-username', USERNAME);
+    await page.type('#login-password', PASSWORD);
+    await page.click('button[type="submit"]');
+    await page.waitForNavigation();
+}
+
+async function signup(page) {
+    await page.goto('http://localhost:8000/register');
+    await page.type('#login-email', EMAIL);
+    await page.type('#login-username', USERNAME);
+    await page.type('#login-password', PASSWORD);
+    await page.click('button[type="submit"]');
+    await page.waitForNavigation();
+}
+
+async function uploadData(page) {
+    await page.click('.SidebarSettingsMenu');
+    await page.click('#import');
+    const [fileChooser] = await Promise.all([
+        page.waitForFileChooser(),
+        page.click('div[aria-label="Import archive"]'),
+    ]);
+    await fileChooser.accept(['./focalboard-internal.boardarchive']);
+    await page.waitForSelector();
+}
