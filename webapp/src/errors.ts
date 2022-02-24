655a241f3581aa5a04d66adc3d60ca1aabd4cc96
wiggin77 <wiggin77@warpmail.net>
2022-02-24 10:19:08 -0500

    typo

--- webapp/src/errors.ts
+++ webapp/src/errors.ts
@@ -47,7 +47,7 @@ function errorDefFromId(id: ErrorId | null): ErrorDef {
         break
     }
     case ErrorId.NotLoggedIn: {
-        errDef.title = intl.formatMessage({id: 'error.not-logged-in', defaultMessage: 'Your session may have expired or you're not logged in. Log in again to access Boards.'})
+        errDef.title = intl.formatMessage({id: 'error.not-logged-in', defaultMessage: 'Your session may have expired or you\'re not logged in. Log in again to access Boards.'})
         errDef.button1Enabled = true
         errDef.button1Text = intl.formatMessage({id: 'error.go-login', defaultMessage: 'Log in'})
         errDef.button1Redirect = '/login'
