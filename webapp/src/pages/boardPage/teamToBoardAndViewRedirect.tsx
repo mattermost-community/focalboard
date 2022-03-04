b0aad97ad83cf2eba4e21c57fd1da575b1666edc
Jesús Espino <jespinog@gmail.com>
2022-03-04 17:37:41 +0100

    fixing the linter

--- webapp/src/pages/boardPage/teamToBoardAndViewRedirect.tsx
+++ webapp/src/pages/boardPage/teamToBoardAndViewRedirect.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {useEffect} from 'react'
+import {useEffect} from 'react'
 import {generatePath, useHistory, useRouteMatch} from 'react-router-dom'
 
 import {getCurrentBoard} from '../../store/boards'
