b61f7b23bfbccc9dd8137a9c818d15a0fc2fc927
Jesús Espino <jespinog@gmail.com>
2022-03-20 18:19:38 +0100

    Fixing linter errors

--- webapp/src/components/centerPanel.tsx
+++ webapp/src/components/centerPanel.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 /* eslint-disable max-lines */
-import React, {useState, useCallback, useRef, useEffect, useMemo} from 'react'
+import React, {useState, useCallback, useEffect, useMemo} from 'react'
 import {useIntl} from 'react-intl'
 import {useHotkeys} from 'react-hotkeys-hook'
 
