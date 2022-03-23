0c9dd1d6e634cfdb62dafdb619bbd4aded44e9e2
Jesús Espino <jespinog@gmail.com>
2022-03-23 18:39:23 +0100

    Simplified the FBRoute

--- webapp/src/route.tsx
+++ webapp/src/route.tsx
@@ -29,18 +29,17 @@ function FBRoute(props: RouteProps) {
     let redirect: React.ReactNode = null
 
     if (Utils.isFocalboardPlugin() && (me?.id !== 'single-user') && props.path !== '/welcome' && loggedIn === true && !me?.props[UserPropPrefix + UserSettingKey.WelcomePageViewed]) {
-        if (props.getOriginalPath) {
-            redirect = ({match}: any) => {
+        redirect = ({match}: any) => {
+            if (props.getOriginalPath) {
                 return <Redirect to={`/welcome?r=${props.getOriginalPath!(match)}`}/>
             }
-        } else {
-            redirect = <Redirect to='/welcome'/>
+            return <Redirect to='/welcome'/>
         }
     }
 
     if (redirect === null && loggedIn === false && props.loginRequired) {
-        if (props.getOriginalPath) {
-            redirect = ({match}: any) => {
+        redirect = ({match}: any) => {
+            if (props.getOriginalPath) {
                 let redirectUrl = '/' + Utils.buildURL(props.getOriginalPath!(match))
                 if (redirectUrl.indexOf('//') === 0) {
                     redirectUrl = redirectUrl.slice(1)
@@ -48,37 +47,20 @@ function FBRoute(props: RouteProps) {
                 const loginUrl = `/error?id=not-logged-in&r=${encodeURIComponent(redirectUrl)}`
                 return <Redirect to={loginUrl}/>
             }
-        } else {
-            redirect = <Redirect to='/error?id=not-logged-in'/>
+            return <Redirect to='/error?id=not-logged-in'/>
         }
     }
 
-    if (redirect !== null) {
-        return (
-            <Route
-                path={props.path}
-                render={props.render}
-                component={props.component}
-                exact={props.exact}
-            >
-                {redirect}
-            </Route>
-        )
-    }
-
-    if (loggedIn === true || !props.loginRequired) {
-        return (
-            <Route
-                path={props.path}
-                render={props.render}
-                component={props.component}
-                exact={props.exact}
-            >
-                {props.children}
-            </Route>
-        )
-    }
-    return null
+    return (
+        <Route
+            path={props.path}
+            render={props.render}
+            component={props.component}
+            exact={props.exact}
+        >
+            {redirect || props.children}
+        </Route>
+    )
 }
 
 export default React.memo(FBRoute)
