c58eee4d852e82c5f1f5b9c2a37f60d8372a3f2f
Jesús Espino <jespinog@gmail.com>
2022-03-29 13:59:11 +0200

    Fixing the sorting of cards taking into consideration the last comment date (#2663)

--- webapp/src/store/comments.ts
+++ webapp/src/store/comments.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {createSlice, PayloadAction, createSelector} from '@reduxjs/toolkit'
 
 import {CommentBlock} from '../blocks/commentBlock'
 
@@ -92,3 +92,17 @@ export function getLastCardComment(cardId: string): (state: RootState) => Commen
         return comments?.[comments?.length - 1]
     }
 }
+
+export const getLastCommentByCard = createSelector(
+    (state: RootState) => state.comments?.commentsByCard || null,
+    (commentsByCard: {[key: string]: CommentBlock[]}|null): {[key: string]: CommentBlock} => {
+        const lastCommentByCard: {[key: string]: CommentBlock} = {}
+        Object.keys(commentsByCard || {}).forEach((cardId) => {
+            if (commentsByCard && commentsByCard[cardId]) {
+                const comments = commentsByCard[cardId]
+                lastCommentByCard[cardId] = comments?.[comments?.length - 1]
+            }
+        })
+        return lastCommentByCard
+    },
+)
