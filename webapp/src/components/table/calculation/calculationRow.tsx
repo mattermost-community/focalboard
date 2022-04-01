f1872e8ce7a77b5416f261dff2ec56229aa38937
Scott Bishel <scott.bishel@mattermost.com>
2022-04-01 14:38:31 -0600

    update tests

--- webapp/src/components/table/calculation/calculationRow.tsx
+++ webapp/src/components/table/calculation/calculationRow.tsx
@@ -1,11 +1,11 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
 
 import {Constants} from '../../../constants'
 
 import './calculationRow.scss'
-import {Board, createBoard, IPropertyTemplate} from '../../../blocks/board'
+import {Board, IPropertyTemplate} from '../../../blocks/board'
 
 import mutator from '../../../mutator'
 import Calculation from '../../calculations/calculation'
@@ -26,6 +26,7 @@ type Props = {
 }
 
 const CalculationRow = (props: Props): JSX.Element => {
+    const {board, cards, activeView, resizingColumn, offset, readonly} = props
     const toggleOptions = (templateId: string, show: boolean) => {
         const newShowOptions = new Map<string, boolean>(showOptions)
         newShowOptions.set(templateId, show)
@@ -37,24 +38,29 @@ const CalculationRow = (props: Props): JSX.Element => {
         id: Constants.titleColumnId,
     } as IPropertyTemplate
 
-    const templates: IPropertyTemplate[] = [
-        titleTemplate,
-        ...props.board.cardProperties.filter((template) => props.activeView.fields.visiblePropertyIds.includes(template.id)),
-    ]
+    // const templates: IPropertyTemplate[] = [
+    //     titleTemplate,
+    //     ...props.board.cardProperties.filter((template) => props.activeView.fields.visiblePropertyIds.includes(template.id)),
+    // ]
 
-    const selectedCalculations = props.activeView.fields.columnCalculations || []
-    
+    const visiblePropertyTemplates = useMemo(() => ([
+        titleTemplate,
+        ...activeView.fields.visiblePropertyIds.map((id) => board.cardProperties.find((t) => t.id === id)).filter((i) => i) as IPropertyTemplate[]
+    ]), [board.cardProperties, activeView.fields.visiblePropertyIds])
+
+    const selectedCalculations = activeView.fields.columnCalculations || []
+
     const [hovered, setHovered] = useState(false)
 
     return (
         <div
             className={'CalculationRow octo-table-row'}
-            onMouseEnter={() => setHovered(!props.readonly)}
+            onMouseEnter={() => setHovered(!readonly)}
             onMouseLeave={() => setHovered(false)}
         >
-            {
-                templates.map((template) => {
-                    const style = {width: columnWidth(props.resizingColumn, props.activeView.fields.columnWidths, props.offset, template.id)}
+            {                
+                visiblePropertyTemplates.map((template) => {
+                    const style = {width: columnWidth(resizingColumn, activeView.fields.columnWidths, offset, template.id)}
                     const defaultValue = template.id === Constants.titleColumnId ? Options.count.value : Options.none.value
                     const value = selectedCalculations[template.id] || defaultValue
 
@@ -62,18 +68,18 @@ const CalculationRow = (props: Props): JSX.Element => {
                         <Calculation
                             key={template.id}
                             style={style}
-                            class={`octo-table-cell ${props.readonly ? 'disabled' : ''}`}
+                            class={`octo-table-cell ${readonly ? 'disabled' : ''}`}
                             value={value}
-                            menuOpen={Boolean(props.readonly ? false : showOptions.get(template.id))}
+                            menuOpen={Boolean(readonly ? false : showOptions.get(template.id))}
                             onMenuClose={() => toggleOptions(template.id, false)}
                             onMenuOpen={() => toggleOptions(template.id, true)}
                             onChange={(v: string) => {
                                 const calculations = {...selectedCalculations}
                                 calculations[template.id] = v
-                                mutator.changeViewColumnCalculations(props.board.id, props.activeView.id, selectedCalculations, calculations, "change column calculation")
+                                mutator.changeViewColumnCalculations(board.id, activeView.id, selectedCalculations, calculations, "change column calculation")
                                 setHovered(false)
                             }}
-                            cards={props.cards}
+                            cards={cards}
                             property={template}
                             hovered={hovered}
                             optionsComponent={TableCalculationOptions}
