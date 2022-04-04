e612cc4971bf8ae018ae67dc37e2ad5db11a3bde
Chen-I Lim <46905241+chenilim@users.noreply.github.com>
2022-04-04 16:48:23 -0700

    Update 000021_fix_categories.down.sql
    Fix Postgres ALTER COLUMN syntax

--- server/services/store/sqlstore/migrations/000021_fix_categories.down.sql
+++ server/services/store/sqlstore/migrations/000021_fix_categories.down.sql
@@ -1,3 +1,7 @@
+{{if .sqlite}}
+ALTER TABLE {{.prefix}}categories DROP COLUMN channel_id VARCHAR(36);
+{{end}}
+
 {{if .mysql}}
 ALTER TABLE {{.prefix}}categories MODIFY COLUMN channel_id VARCHAR(36) NOT NULL;
 ALTER TABLE {{.prefix}}categories MODIFY COLUMN id INT NOT NULL UNIQUE AUTO_INCREMENT;
@@ -5,10 +9,9 @@ ALTER TABLE {{.prefix}}category_blocks MODIFY COLUMN id INT NOT NULL UNIQUE AUTO
 {{end}}
 
 {{if .postgres}}
-ALTER TABLE {{.prefix}}category_blocks MODIFY COLUMN id SERIAL NOT NULL;
-ALTER TABLE {{.prefix}}categories MODIFY COLUMN channel_id VARCHAR(36) NOT NULL;
-{{end}}
-
-{{if .sqlite}}
-ALTER TABLE {{.prefix}}categories DROP COLUMN channel_id VARCHAR(36);
+ALTER TABLE {{.prefix}}categories ALTER COLUMN id DROP NOT NULL;
+ALTER TABLE {{.prefix}}categories ALTER COLUMN id TYPE SERIAL;
+ALTER TABLE {{.prefix}}category_blocks ALTER COLUMN id DROP NOT NULL;
+ALTER TABLE {{.prefix}}category_blocks ALTER COLUMN id TYPE SERIAL;
+ALTER TABLE {{.prefix}}categories ALTER COLUMN channel_id TYPE VARCHAR(32);
 {{end}}
