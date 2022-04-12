f4b1e6006fdcdd823d7996bde723e7129aab8e75
Scott Bishel <scott.bishel@mattermost.com>
2022-04-12 15:40:50 -0600

    Update server/services/store/sqlstore/migrations/000017_add_teams_and_boards.up.sql
    Co-authored-by: Miguel de la Cruz <miguel@mcrx.me>

--- server/services/store/sqlstore/migrations/000017_add_teams_and_boards.up.sql
+++ server/services/store/sqlstore/migrations/000017_add_teams_and_boards.up.sql
@@ -18,7 +18,7 @@ UPDATE {{.prefix}}blocks SET fields = JSON_SET(fields, '$.columnCalculations', c
 UPDATE {{.prefix}}blocks b
   JOIN (
     SELECT id, fields->'$.columnCalculations' as board_calculations from {{.prefix}}blocks
-    WHERE fields -> '$.columnCalculations' <> cast('{}' as json) and fields -> '$.columnCalculations' <> cast('[]' as json)
+    WHERE fields -> '$.columnCalculations' <> cast('{}' as json)
   ) AS s on s.id = b.root_id
   SET fields = JSON_SET(fields, '$.columnCalculations', cast(s.board_calculations as json))
   WHERE b.fields->'$.viewType' = 'table'
