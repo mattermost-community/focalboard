9c03838840d1392d613fc8bcfe9b0f1d9d346bf2
Miguel de la Cruz <miguel@mcrx.me>
2022-04-13 14:21:25 +0200

    Use the correct endpoint for exporting all boards on a team

--- webapp/src/octoClient.ts
+++ webapp/src/octoClient.ts
@@ -201,9 +201,13 @@ class OctoClient {
         return (await this.getJson(response, {})) as Record<string, string>
     }
 
-    // If no boardID is provided, it will export the entire archive
-    async exportArchive(boardID = ''): Promise<Response> {
+    async exportBoardArchive(boardID: string): Promise<Response> {
         const path = `/api/v1/boards/${boardID}/archive/export`
+        return fetch(this.getBaseURL() + path, {headers: this.headers()})
+    }
+
+    async exportFullArchive(teamID: string): Promise<Response> {
+        const path = `/api/v1/teams/${teamID}/archive/export`
         return fetch(this.getBaseURL() + path, {headers: this.headers()})
     }
 
