4c61ae96234f41ab1f1e7f99757f231106a4e42d
Miguel de la Cruz <miguel@mcrx.me>
2022-04-13 22:24:32 +0200

    Update focalboard endpoints to v2 namespace (#2781)

--- server/main/doc.go
+++ server/main/doc.go
@@ -4,7 +4,7 @@
 //
 //     Schemes: http, https
 //     Host: localhost
-//     BasePath: /api/v1
+//     BasePath: /api/v2
 //     Version: 1.0.0
 //     License: Custom https://github.com/mattermost/focalboard/blob/main/LICENSE.txt
 //     Contact: Focalboard<api@focalboard.com> https://www.focalboard.com
