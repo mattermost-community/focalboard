08d839b38daeec239480ec6b6b271eaac22f33ee
Scott Bishel <scott.bishel@mattermost.com>
2022-04-27 15:00:11 -0600

    clean up test

--- webapp/src/components/sidebar/registrationLink.test.tsx
+++ webapp/src/components/sidebar/registrationLink.test.tsx
@@ -10,46 +10,16 @@ import {render} from '@testing-library/react'
 
 import {wrapIntl} from '../../testUtils'
 
-import {TestBlockFactory} from '../../test/testBlockFactory'
-
 import RegistrationLink from './registrationLink'
 
 describe('components/sidebar/RegistrationLink', () => {
     const mockStore = configureStore([])
-
-    const board = TestBlockFactory.createBoard()
-    board.id = 'board1'
-
-    const categoryAttribute1 = TestBlockFactory.createCategoryBoards()
-    categoryAttribute1.name = 'Category 1'
-    categoryAttribute1.boardIDs = [board.id]
-
     const state = {
         teams: {
             current: {
                 id: 'team-id',
                 signupToken: 'abc123'
             },
-        },
-        boards: {
-            current: board.id,
-            boards: {
-                [board.id]: board,
-            },
-            myBoardMemberships: {
-                [board.id]: board,
-            },
-        },
-        views: {
-            views: [],
-        },
-        users: {
-            me: {},
-        },
-        sidebar: {
-            categoryAttributes: [
-                categoryAttribute1,
-            ],
         },
     }
 
