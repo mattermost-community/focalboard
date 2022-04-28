f4541630e7fcf58521d48a49fd0fe88c050daf9e
Scott Bishel <scott.bishel@mattermost.com>
2022-04-28 14:41:38 -0600

    update for v0.17.0 (#2943)

--- mac/Focalboard/Globals.swift
+++ mac/Focalboard/Globals.swift
@@ -4,7 +4,7 @@
 import Foundation
 
 class Globals {
-	static let ProductVersion = 01600
+	static let ProductVersion = 01700
 	static let WhatsNewVersion = 01100
 
 	static var currentWhatsNewVersion: Int {
