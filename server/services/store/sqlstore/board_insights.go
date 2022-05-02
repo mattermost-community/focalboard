c092334ef47b3da168bec6c055f11a272a74ddf1
Shivashis Padhi <shivashispadhi@gmail.com>
2022-05-02 19:20:15 +0530

    Modify queries to work with sqlite, postgres, mysql

--- server/services/store/sqlstore/board_insights.go
+++ server/services/store/sqlstore/board_insights.go
@@ -19,14 +19,14 @@ func (s *SQLStore) getTeamBoardsInsights(db sq.BaseRunner, teamID string, durati
 	*/
 
 	boardsHistoryQuery := s.getQueryBuilder(db).Select("boards.id", "boards.title", "count(boards_history.id) as count", "boards_history.modified_by", "boards.created_by").
-		From("focalboard_boards_history as boards_history").
-		Join("focalboard_boards as boards on boards_history.id = boards.id").
-		Where(fmt.Sprintf("boards_history.insert_at > now() - interval '%s' and boards.team_id = '%s' and boards_history.modified_by != 'system' and boards.delete_at = 0", duration, teamID)).
+		From(s.tablePrefix + "boards_history as boards_history").
+		Join(s.tablePrefix + "boards as boards on boards_history.id = boards.id").
+		Where(fmt.Sprintf("boards_history.insert_at > %s and boards.team_id = '%s' and boards_history.modified_by != 'system' and boards.delete_at = 0", s.durationSelector(duration), teamID)).
 		GroupBy("boards_history.id, boards.id, boards_history.modified_by")
 	blocksHistoryQuery := s.getQueryBuilder(db).Select("boards.id", "boards.title", "count(blocks_history.id) as count", "blocks_history.modified_by", "boards.created_by").
-		From("focalboard_blocks_history as blocks_history").
-		Join("focalboard_boards as boards on blocks_history.board_id = boards.id").
-		Where(fmt.Sprintf("blocks_history.insert_at > now() - interval '%s' and boards.team_id = '%s' and blocks_history.modified_by != 'system' and boards.delete_at = 0", duration, teamID)).
+		From(s.tablePrefix + "blocks_history as blocks_history").
+		Join(s.tablePrefix + "boards as boards on blocks_history.board_id = boards.id").
+		Where(fmt.Sprintf("blocks_history.insert_at > %s and boards.team_id = '%s' and blocks_history.modified_by != 'system' and boards.delete_at = 0", s.durationSelector(duration), teamID)).
 		GroupBy("blocks_history.id, boards.id, blocks_history.modified_by")
 	blocksHistoryQueryString, blocksHistoryQueryargs, err := blocksHistoryQuery.ToSql()
 
@@ -34,11 +34,11 @@ func (s *SQLStore) getTeamBoardsInsights(db sq.BaseRunner, teamID string, durati
 		return nil, err
 	}
 	boardsAndBlocksHistoryQuery := boardsHistoryQuery.Suffix("UNION ALL "+blocksHistoryQueryString, blocksHistoryQueryargs...)
-	insights := s.getQueryBuilder(db).Select("id", "title", "sum(count) as activity_count", "string_agg(distinct modified_by, ',') as active_users", "created_by").
+	insights := s.getQueryBuilder(db).Select("id", "title", "sum(count) as activity_count", fmt.Sprintf("%s as active_users", s.concatenationSelector("distinct modified_by", ",")), "created_by").
 		FromSelect(boardsAndBlocksHistoryQuery, "boards_and_blocks_history").
 		GroupBy("id, title, created_by").
 		OrderBy("activity_count desc").
-		Limit(4)
+		Limit(100)
 
 	rows, err := insights.Query()
 
@@ -64,14 +64,14 @@ func (s *SQLStore) getUserBoardsInsights(db sq.BaseRunner, userID string, durati
 	*/
 
 	boardsHistoryQuery := s.getQueryBuilder(db).Select("boards.id", "boards.title", "count(boards_history.id) as count", "boards_history.modified_by", "boards.created_by").
-		From("focalboard_boards_history as boards_history").
-		Join("focalboard_boards as boards on boards_history.id = boards.id").
-		Where(fmt.Sprintf("boards_history.insert_at > now() - interval '%s' and boards_history.modified_by != 'system' and boards.delete_at = 0", duration)).
+		From(s.tablePrefix + "boards_history as boards_history").
+		Join(s.tablePrefix + "boards as boards on boards_history.id = boards.id").
+		Where(fmt.Sprintf("boards_history.insert_at > %s and boards_history.modified_by != 'system' and boards.delete_at = 0", s.durationSelector(duration))).
 		GroupBy("boards_history.id, boards.id, boards_history.modified_by")
 	blocksHistoryQuery := s.getQueryBuilder(db).Select("boards.id", "boards.title", "count(blocks_history.id) as count", "blocks_history.modified_by", "boards.created_by").
-		From("focalboard_blocks_history as blocks_history").
-		Join("focalboard_boards as boards on blocks_history.board_id = boards.id").
-		Where(fmt.Sprintf("blocks_history.insert_at > now() - interval '%s' and blocks_history.modified_by != 'system' and boards.delete_at = 0", duration)).
+		From(s.tablePrefix + "blocks_history as blocks_history").
+		Join(s.tablePrefix + "boards as boards on blocks_history.board_id = boards.id").
+		Where(fmt.Sprintf("blocks_history.insert_at > %s and blocks_history.modified_by != 'system' and boards.delete_at = 0", s.durationSelector(duration))).
 		GroupBy("blocks_history.id, boards.id, blocks_history.modified_by")
 	blocksHistoryQueryString, blocksHistoryQueryargs, err := blocksHistoryQuery.ToSql()
 
@@ -79,17 +79,16 @@ func (s *SQLStore) getUserBoardsInsights(db sq.BaseRunner, userID string, durati
 		return nil, err
 	}
 	boardsAndBlocksHistoryQuery := boardsHistoryQuery.Suffix("UNION ALL "+blocksHistoryQueryString, blocksHistoryQueryargs...)
-	insights := s.getQueryBuilder(db).Select("id", "title", "sum(count) as activity_count", "string_agg(distinct modified_by, ',') as active_users", "created_by").
+	insights := s.getQueryBuilder(db).Select("id", "title", "sum(count) as activity_count", fmt.Sprintf("%s as active_users", s.concatenationSelector("distinct modified_by", ",")), "created_by").
 		FromSelect(boardsAndBlocksHistoryQuery, "boards_and_blocks_history").
 		GroupBy("id, title, created_by").
-		OrderBy("activity_count desc").
-		Limit(4)
+		OrderBy("activity_count desc")
 
 	userInsights := s.getQueryBuilder(db).Select("*").
 		FromSelect(insights, "insights").
 		// TODO: clean the following where clause
-		Where(fmt.Sprintf("created_by = '%s' or position('%s' in active_users) > 0", userID, userID)).
-		Limit(4)
+		Where(fmt.Sprintf("created_by = '%s' or %s", userID, s.elementInColumn(userID, "active_users"))).
+		Limit(100)
 
 	rows, err := userInsights.Query()
 
