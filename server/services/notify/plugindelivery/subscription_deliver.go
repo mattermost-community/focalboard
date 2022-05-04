1ff28087f8fb03d5abcb2d5184ded8e70cfc60e3
Doug Lauder <wiggin77@warpmail.net>
2022-05-04 16:10:26 -0400

    GH-2469 Add board-bot to team (#3026)
    * add bot to team before any mention or subscription notification

--- server/services/notify/plugindelivery/subscription_deliver.go
+++ server/services/notify/plugindelivery/subscription_deliver.go
@@ -17,7 +17,7 @@ var (
 )
 
 // SubscriptionDeliverSlashAttachments notifies a user that changes were made to a block they are subscribed to.
-func (pd *PluginDelivery) SubscriptionDeliverSlackAttachments(subscriberID string, subscriptionType model.SubscriberType,
+func (pd *PluginDelivery) SubscriptionDeliverSlackAttachments(teamID string, subscriberID string, subscriptionType model.SubscriberType,
 	attachments []*mm_model.SlackAttachment) error {
 	// check subscriber is member of channel
 	_, err := pd.api.GetUserByID(subscriberID)
@@ -29,7 +29,7 @@ func (pd *PluginDelivery) SubscriptionDeliverSlackAttachments(subscriberID strin
 		return fmt.Errorf("cannot fetch channel member for user %s: %w", subscriberID, err)
 	}
 
-	channelID, err := pd.getDirectChannelID(subscriberID, subscriptionType, pd.botID)
+	channelID, err := pd.getDirectChannelID(teamID, subscriberID, subscriptionType, pd.botID)
 	if err != nil {
 		return err
 	}
@@ -44,14 +44,14 @@ func (pd *PluginDelivery) SubscriptionDeliverSlackAttachments(subscriberID strin
 	return pd.api.CreatePost(post)
 }
 
-func (pd *PluginDelivery) getDirectChannelID(subscriberID string, subscriberType model.SubscriberType, botID string) (string, error) {
+func (pd *PluginDelivery) getDirectChannelID(teamID string, subscriberID string, subscriberType model.SubscriberType, botID string) (string, error) {
 	switch subscriberType {
 	case model.SubTypeUser:
 		user, err := pd.api.GetUserByID(subscriberID)
 		if err != nil {
 			return "", fmt.Errorf("cannot find user: %w", err)
 		}
-		channel, err := pd.api.GetDirectChannel(user.Id, botID)
+		channel, err := pd.getDirectChannel(teamID, user.Id, botID)
 		if err != nil {
 			return "", fmt.Errorf("cannot get direct channel: %w", err)
 		}
@@ -61,4 +61,13 @@ func (pd *PluginDelivery) getDirectChannelID(subscriberID string, subscriberType
 	default:
 		return "", ErrUnsupportedSubscriberType
 	}
+}
+
+func (pd *PluginDelivery) getDirectChannel(teamID string, userID string, botID string) (*mm_model.Channel, error) {
+	// first ensure the bot is a member of the team.
+	_, err := pd.api.CreateMember(teamID, botID)
+	if err != nil {
+		return nil, fmt.Errorf("cannot add bot to team %s: %w", teamID, err)
+	}
+	return pd.api.GetDirectChannel(userID, botID)
 }
