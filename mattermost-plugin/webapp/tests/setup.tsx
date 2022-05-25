2a35ca2a82fb8993206e33d5ecc8cb52b7290a64
Paul Esch-Laurent <paul.esch-laurent@mattermost.com>
2022-05-25 14:39:07 -0500

    test: only include MM webapp test setup in not CI

--- mattermost-plugin/webapp/tests/setup.tsx
+++ mattermost-plugin/webapp/tests/setup.tsx
@@ -1,4 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import 'mattermost-webapp/tests/setup'
+// This won't exist locally when running in CI
+// eslint-disable-next-line no-process-env
+if (!process.env.CI) {
+    require('mattermost-webapp/tests/setup')
+}
