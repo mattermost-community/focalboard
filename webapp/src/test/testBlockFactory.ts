949886e9c1484954997e2ea62f5e850a785e1921
Jesús Espino <jespinog@gmail.com>
2022-06-02 20:17:21 +0200

    Fixing some tests type checks adding the roles attribute to the users

--- webapp/src/test/testBlockFactory.ts
+++ webapp/src/test/testBlockFactory.ts
@@ -192,6 +192,7 @@ class TestBlockFactory {
             create_at: Date.now(),
             update_at: Date.now(),
             is_bot: false,
+            roles: 'system_user',
         }
     }
 }
