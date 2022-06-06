6e8ae98653f9805d44467ac7dd537d4fccdb4f3c
Harshil Sharma <harshilsharma63@gmail.com>
2022-06-06 10:38:52 +0530

    Generated mocks

--- server/go.tools.mod
+++ server/go.tools.mod
@@ -2,7 +2,12 @@ module github.com/mattermost/focalboard/server
 
 go 1.18
 
+require github.com/golang/mock v1.6.0
+
 require (
-	github.com/golang/mock v1.6.0
 	github.com/jteeuwen/go-bindata v3.0.7+incompatible // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- server/services/permissions/mmpermissions/mocks/mockpluginapi.go
+++ server/services/permissions/mmpermissions/mocks/mockpluginapi.go
@@ -681,6 +681,21 @@ func (mr *MockAPIMockRecorder) GetChannelsForTeamForUser(arg0, arg1, arg2 interf
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetChannelsForTeamForUser", reflect.TypeOf((*MockAPI)(nil).GetChannelsForTeamForUser), arg0, arg1, arg2)
 }
 
+// GetCloudLimits mocks base method.
+func (m *MockAPI) GetCloudLimits() (*model.ProductLimits, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetCloudLimits")
+	ret0, _ := ret[0].(*model.ProductLimits)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetCloudLimits indicates an expected call of GetCloudLimits.
+func (mr *MockAPIMockRecorder) GetCloudLimits() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetCloudLimits", reflect.TypeOf((*MockAPI)(nil).GetCloudLimits))
+}
+
 // GetCommand mocks base method.
 func (m *MockAPI) GetCommand(arg0 string) (*model.Command, error) {
 	m.ctrl.T.Helper()
