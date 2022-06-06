002a53aedb54d3f847e03c31c40f2a2fccc8dc9a
Harshil Sharma <harshilsharma63@gmail.com>
2022-06-06 11:48:59 +0530

    added tests

--- server/services/store/storetests/files.go
+++ server/services/store/storetests/files.go
@@ -12,20 +12,26 @@ func StoreTestFileStore(t *testing.T, setup func(t *testing.T) (store.Store, fun
 	sqlStore, tearDown := setup(t)
 	defer tearDown()
 
-	t.Run("test saveFileInfo", func(t *testing.T) {
-		t.Run("should save fileinfo", func(t *testing.T) {
-			fileInfo := &mmModel.FileInfo{
-				Id:        "file_info_1",
-				CreateAt:  utils.GetMillis(),
-				Name:      "Dunder Mifflin Sales Report 2022",
-				Extension: ".salez",
-				Size:      112233,
-				DeleteAt:  0,
-				Archived:  false,
-			}
+	t.Run("should save and retrieve fileinfo", func(t *testing.T) {
+		fileInfo := &mmModel.FileInfo{
+			Id:        "file_info_1",
+			CreateAt:  utils.GetMillis(),
+			Name:      "Dunder Mifflin Sales Report 2022",
+			Extension: ".sales",
+			Size:      112233,
+			DeleteAt:  0,
+		}
 
-			err := sqlStore.SaveFileInfo(fileInfo)
-			assert.NoError(t, err)
-		})
+		err := sqlStore.SaveFileInfo(fileInfo)
+		assert.NoError(t, err)
+
+		retrievedFileInfo, err := sqlStore.GetFileInfo("file_info_1")
+		assert.NoError(t, err)
+		assert.Equal(t, "file_info_1", retrievedFileInfo.Id)
+		assert.Equal(t, "Dunder Mifflin Sales Report 2022", retrievedFileInfo.Name)
+		assert.Equal(t, ".sales", retrievedFileInfo.Extension)
+		assert.Equal(t, int64(112233), retrievedFileInfo.Size)
+		assert.Equal(t, int64(0), retrievedFileInfo.DeleteAt)
+		assert.False(t, retrievedFileInfo.Archived)
 	})
 }
