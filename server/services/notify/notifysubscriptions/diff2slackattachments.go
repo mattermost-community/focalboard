dc28ad7d6f3b20638411452c47e6c1422663fbe0
Rajat Dabade <rajat.dabade@mattermost.com>
2022-06-15 21:01:13 +0530

    Update

--- server/services/notify/notifysubscriptions/diff2slackattachments.go
+++ server/services/notify/notifysubscriptions/diff2slackattachments.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	// card change notifications.
-	defAddCardNotify    = "{{.Authors | printAuthors \"unknown_user\" }} has added the card {{. | makeLink}}\n"
+	defAddCardNotify = "{{.Authors | printAuthors \"unknown_user\" }} has added the card {{. | makeLink}}\n"
 	//nolint:lll
 	defModifyCardNotify = "###### {{.Authors | printAuthors \"unknown_user\" }} has modified the card {{. | makeLink}} on the board {{. | makeBoardLink}}\n"
 	defDeleteCardNotify = "{{.Authors | printAuthors \"unknown_user\" }} has deleted the card {{. | makeLink}}\n"
