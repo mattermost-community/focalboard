094445948b4bca0d21cd3cd87dc7e2c0ef6e47fa
Jesús Espino <jespinog@gmail.com>
2022-06-20 17:09:14 +0200

    Fixing a typing problem

--- webapp/src/components/messages/cloudMessage.test.tsx
+++ webapp/src/components/messages/cloudMessage.test.tsx
@@ -142,6 +142,7 @@ describe('components/messages/CloudMessage', () => {
             create_at: 0,
             update_at: Date.now() - (1000 * 60 * 60 * 24), //24 hours,
             is_bot: false,
+            roles: 'system_user',
         }
         const state = {
             users: {
