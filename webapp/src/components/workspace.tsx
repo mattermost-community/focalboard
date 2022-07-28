1bbf44b25e517b4f4220c6ad4b2527f7eaf106f6
Paul Esch-Laurent <paul.esch-laurent@mattermost.com>
2022-07-28 15:52:11 -0500

    fix(webapp): update template selector text

--- webapp/src/components/workspace.tsx
+++ webapp/src/components/workspace.tsx
@@ -115,18 +115,13 @@ function CenterContent(props: Props) {
             title={
                 <FormattedMessage
                     id='BoardTemplateSelector.plugin.no-content-title'
-                    defaultMessage='Create a Board in {teamName}'
-                    values={{teamName: team?.title}}
+                    defaultMessage='Create a board'
                 />
             }
             description={
                 <FormattedMessage
                     id='BoardTemplateSelector.plugin.no-content-description'
-                    defaultMessage='Add a board to the sidebar using any of the templates defined below or start from scratch.{lineBreak} Members of "{teamName}" will have access to boards created here.'
-                    values={{
-                        teamName: <b>{team?.title}</b>,
-                        lineBreak: <br/>,
-                    }}
+                    defaultMessage='Add a board to the sidebar using any of the templates defined below or start from scratch.'
                 />
             }
             channelId={match.params.channelId}
