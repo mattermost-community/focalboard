ee395d49b1edb5631467377ac4c94b558db48b7a
Rajat Dabade <rajat.dabade@mattermost.com>
2022-07-30 01:58:00 +0530

    Fetching board member in parellel (#3379)
    * Translated using Weblate (Malayalam)
    
    Currently translated at 100.0% (303 of 303 strings)
    
    Translation: Focalboard/webapp
    Translate-URL: https://translate.mattermost.com/projects/focalboard/webapp/ml/
    
    * Update translation files
    
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: Focalboard/webapp
    Translate-URL: https://translate.mattermost.com/projects/focalboard/webapp/
    
    Update translation files
    
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: Focalboard/webapp
    Translate-URL: https://translate.mattermost.com/projects/focalboard/webapp/
    
    Update translation files
    
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: Focalboard/webapp
    Translate-URL: https://translate.mattermost.com/projects/focalboard/webapp/
    
    * Translated using Weblate (German)
    
    Currently translated at 100.0% (312 of 312 strings)
    
    Translation: Focalboard/webapp
    Translate-URL: https://translate.mattermost.com/projects/focalboard/webapp/de/
    
    * Added code for fetching all the users at one time.
    
    * Added the code for personal server users
    
    * Update-code
    
    * Linter fixes
    
    * Test cases updated, some minor changes
    
    * Update the code according to review comments
    
    * Cypress test fix
    
    * Update server/services/store/mattermostauthlayer/mattermostauthlayer.go
    
    Co-authored-by: Varghese Jose <varghese.jose@tutanota.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: jprusch <rs@schaeferbarthold.de>
    Co-authored-by: Scott Bishel <scott.bishel@mattermost.com>

--- server/services/store/mattermostauthlayer/mattermostauthlayer.go
+++ server/services/store/mattermostauthlayer/mattermostauthlayer.go
@@ -295,6 +295,28 @@ func (s *MattermostAuthLayer) GetUsersByTeam(teamID string) ([]*model.User, erro
 	return users, nil
 }
 
+func (s *MattermostAuthLayer) GetUsersList(userIDs []string) ([]*model.User, error) {
+	query := s.getQueryBuilder().
+		Select("u.id", "u.username", "u.email", "u.nickname", "u.firstname", "u.lastname", "u.props", "u.CreateAt as create_at", "u.UpdateAt as update_at",
+			"u.DeleteAt as delete_at", "b.UserId IS NOT NULL AS is_bot").
+		From("Users as u").
+		LeftJoin("Bots b ON ( b.UserId = Users.ID )").
+		Where(sq.Eq{"u.id": userIDs})
+
+	rows, err := query.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer s.CloseRows(rows)
+
+	users, err := s.usersFromRows(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
 func (s *MattermostAuthLayer) SearchUsersByTeam(teamID string, searchQuery string) ([]*model.User, error) {
 	query := s.getQueryBuilder().
 		Select("u.id", "u.username", "u.email", "u.nickname", "u.firstname", "u.lastname", "u.props", "u.CreateAt as create_at", "u.UpdateAt as update_at",
