858c6f0bed4c4027607f9ba6c3accdc9646c2169
Jesús Espino <jespinog@gmail.com>
2022-08-01 12:14:17 +0200

    Only show the channel unlink option if you have the right permissions (#3430)
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    (cherry picked from commit b31f2afe811c4c78d84606848569881b490bcddb)

--- webapp/src/components/shareBoard/channelPermissionsRow.tsx
+++ webapp/src/components/shareBoard/channelPermissionsRow.tsx
@@ -14,12 +14,15 @@ import {Channel} from '../../store/channels'
 import {Utils} from '../../utils'
 import mutator from '../../mutator'
 import octoClient from '../../octoClient'
+import {Permission} from '../../constants'
 
 import PrivateIcon from '../../widgets/icons/lockOutline'
 import PublicIcon from '../../widgets/icons/globe'
 import DeleteIcon from '../../widgets/icons/delete'
 import CompassIcon from '../../widgets/icons/compassIcon'
 import ConfirmationDialogBox from "../confirmationDialogBox"
+
+import BoardPermissionGate from '../permissions/boardPermissionGate'
 
 const ChannelPermissionsRow = (): JSX.Element => {
     const intl = useIntl()
@@ -80,26 +83,37 @@ const ChannelPermissionsRow = (): JSX.Element => {
                 <div className='ml-3'><strong>{linkedChannel.display_name}</strong></div>
             </div>
             <div>
-                <MenuWrapper>
-                    <button className='user-item__button'>
-                        <FormattedMessage
-                            id='BoardMember.schemeEditor'
-                            defaultMessage='Editor'
-                        />
-                        <CompassIcon
-                            icon='chevron-down'
-                            className='CompassIcon'
-                        />
-                    </button>
-                    <Menu position='left'>
-                        <Menu.Text
-                            id='Unlink'
-                            icon={<DeleteIcon/>}
-                            name={intl.formatMessage({id: 'BoardMember.unlinkChannel', defaultMessage: 'Unlink'})}
-                            onClick={() => setShowUnlinkChannelConfirmation(true)}
-                        />
-                    </Menu>
-                </MenuWrapper>
+                <BoardPermissionGate permissions={[Permission.ManageBoardRoles]}>
+                    <MenuWrapper>
+                        <button className='user-item__button'>
+                            <FormattedMessage
+                                id='BoardMember.schemeEditor'
+                                defaultMessage='Editor'
+                            />
+                            <CompassIcon
+                                icon='chevron-down'
+                                className='CompassIcon'
+                            />
+                        </button>
+                        <Menu position='left'>
+                            <Menu.Text
+                                id='Unlink'
+                                icon={<DeleteIcon/>}
+                                name={intl.formatMessage({id: 'BoardMember.unlinkChannel', defaultMessage: 'Unlink'})}
+                                onClick={() => setShowUnlinkChannelConfirmation(true)}
+                            />
+                        </Menu>
+                    </MenuWrapper>
+                </BoardPermissionGate>
+                <BoardPermissionGate
+                    permissions={[Permission.ManageBoardRoles]}
+                    invert={true}
+                >
+                    <FormattedMessage
+                        id='BoardMember.schemeEditor'
+                        defaultMessage='Editor'
+                    />
+                </BoardPermissionGate>
             </div>
         </div>
     )
