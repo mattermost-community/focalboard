014147697bd882d121c0a9c9bde3c9d81bd4928e
kamre <eremchenko@gmail.com>
2022-08-05 23:32:58 +0300

    Unit tests for `FilterValue` and `ViewHeaderPropertiesMenu` checking that menu is not closed added.

--- webapp/src/components/viewHeader/viewHeaderPropertiesMenu.test.tsx
+++ webapp/src/components/viewHeader/viewHeaderPropertiesMenu.test.tsx
@@ -93,4 +93,28 @@ describe('components/viewHeader/viewHeaderPropertiesMenu', () => {
             [...activeView.fields.visiblePropertyIds, Constants.badgesColumnId],
         )
     })
+    test('show menu and verify that it is not closed after clicking on the item', () => {
+        render(
+            wrapIntl(
+                <ReduxProvider store={store}>
+                    <ViewHeaderPropertiesMenu
+                        activeView={activeView}
+                        properties={board.cardProperties}
+                    />
+                </ReduxProvider>,
+            ),
+        )
+        const menuButton = screen.getByRole('button', {name: 'Properties menu'})
+        userEvent.click(menuButton)
+
+        const property1Button = screen.getByRole('button', {name: 'Property 1'})
+        userEvent.click(property1Button)
+        expect(property1Button).toBeInTheDocument()
+
+        const property2Button = screen.getByRole('button', {name: 'Property 2'})
+        userEvent.click(property2Button)
+        expect(property2Button).toBeInTheDocument()
+
+        expect(mockedMutator.changeViewVisibleProperties).toHaveBeenCalledTimes(2)
+    })
 })
