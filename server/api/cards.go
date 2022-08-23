09b6c896729036f8f2ef6480bbc90bea83ae5229
wiggin77 <wiggin77@warpmail.net>
2022-08-22 22:45:50 -0400

    register GetCard API

--- server/api/cards.go
+++ server/api/cards.go
@@ -16,6 +16,7 @@ func (a *API) registerCardsRoutes(r *mux.Router) {
 	// Cards APIs
 	r.HandleFunc("/boards/{boardID}/cards", a.sessionRequired(a.handleCreateCard)).Methods("POST")
 	r.HandleFunc("/cards/{cardID}/cards", a.sessionRequired(a.handlePatchCard)).Methods("POST")
+	r.HandleFunc("/cards/{cardID}", a.sessionRequired(a.handleGetCard)).Methods("GET")
 }
 
 func (a *API) handleCreateCard(w http.ResponseWriter, r *http.Request) {
