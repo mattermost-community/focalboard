8978a4ef012af2a87265c951fcc8f54d6c4ce709
Scott Bishel <scott.bishel@mattermost.com>
2022-10-06 17:17:19 -0600

    change variable name

--- webapp/src/properties/multiperson/multiperson.tsx
+++ webapp/src/properties/multiperson/multiperson.tsx
@@ -110,15 +110,15 @@ const MultiPerson = (props: PropertyProps): JSX.Element => {
     }
 
     const addUser = useCallback(async (userId: string, role: string) => {
-        const minimumRole = role || MemberRole.Viewer
+        const newRole = role || MemberRole.Viewer
         const newMember = {
             boardId: board.id,
             userId,
             roles: role,
-            schemeAdmin: minimumRole === MemberRole.Admin,
-            schemeEditor: minimumRole === MemberRole.Admin || MemberRole.Editor,
-            schemeCommenter: minimumRole === MemberRole.Admin || MemberRole.Editor || minimumRole === MemberRole.Commenter,
-            schemeViewer: minimumRole === MemberRole.Admin || MemberRole.Editor || minimumRole === MemberRole.Commenter || minimumRole === MemberRole.Viewer,
+            schemeAdmin: newRole === MemberRole.Admin,
+            schemeEditor: newRole === MemberRole.Admin || MemberRole.Editor,
+            schemeCommenter: newRole === MemberRole.Admin || MemberRole.Editor || newRole === MemberRole.Commenter,
+            schemeViewer: newRole === MemberRole.Admin || MemberRole.Editor || newRole === MemberRole.Commenter || newRole === MemberRole.Viewer,
         } as BoardMember
 
         setConfirmAddUser(null)
