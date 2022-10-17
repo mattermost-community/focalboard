0ed1fa2f7389213a9a6e094e8455d76aacdc2bd4
Scott Bishel <scott.bishel@mattermost.com>
2022-10-17 16:14:19 -0600

    lint fixes

--- server/services/store/mattermostauthlayer/mattermostauthlayer_test.go
+++ server/services/store/mattermostauthlayer/mattermostauthlayer_test.go
@@ -13,25 +13,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var errTest = errors.New("failed to patch bot")
+
 func TestGetBoardsBotID(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	servicesAPI := mockservicesapi.NewMockServicesAPI(ctrl)
 
 	mmAuthLayer, _ := New("test", nil, nil, mlog.CreateConsoleTestLogger(true, mlog.LvlError), servicesAPI, "")
 
-	servicesAPI.EXPECT().EnsureBot(model.FocalboardBot).Return("", errors.New("failed to patch bot"))
+	servicesAPI.EXPECT().EnsureBot(model.FocalboardBot).Return("", errTest)
 	_, err := mmAuthLayer.getBoardsBotID()
 	require.NotEmpty(t, err)
 
 	servicesAPI.EXPECT().EnsureBot(model.FocalboardBot).Return("TestBotID", nil).Times(1)
-	botId, err := mmAuthLayer.getBoardsBotID()
+	botID, err := mmAuthLayer.getBoardsBotID()
 	require.Empty(t, err)
-	require.NotEmpty(t, botId)
-	require.Equal(t, "TestBotID", botId)
+	require.NotEmpty(t, botID)
+	require.Equal(t, "TestBotID", botID)
 
 	// Call again, should not call "EnsureBot"
-	botId, err = mmAuthLayer.getBoardsBotID()
+	botID, err = mmAuthLayer.getBoardsBotID()
 	require.Empty(t, err)
-	require.NotEmpty(t, botId)
-	require.Equal(t, "TestBotID", botId)
+	require.NotEmpty(t, botID)
+	require.Equal(t, "TestBotID", botID)
 }
