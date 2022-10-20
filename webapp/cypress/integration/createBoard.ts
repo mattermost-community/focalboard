50e24a385553508162f90a21042fd59af4299c5e
Harshil Sharma <harshilsharma63@gmail.com>
2022-10-20 15:07:44 +0530

    reverting test fix

--- webapp/cypress/integration/createBoard.ts
+++ webapp/cypress/integration/createBoard.ts
@@ -136,7 +136,7 @@ describe('Create and delete board / card', () => {
         cy.get('.Sidebar .octo-sidebar-list').then((el) => {
             cy.log(el.text())
         })
-        cy.get('.Sidebar .octo-sidebar-title').
+        cy.get('.Sidebar .octo-sidebar-list').
             contains(boardTitle).
             parent().
             find('.MenuWrapper').
