69198285bf56c2b9a84bc78d06b831ec21594db3
Harshil Sharma <harshilsharma63@gmail.com>
2022-11-11 18:07:53 +0530

    WIP

--- server/services/store/sqlstore/migrations/000032_move_boards_categoyr_to_last_if_exist.up.sql
+++
@@ -1,1 +0,0 @@
-update focalboard_categories fc set sort_order = (select 10 * (select count(*) from focalboard_categories group by user_id, team_id having user_id = fc.user_id and team_id = fc.team_id)) where name = 'Boards';
