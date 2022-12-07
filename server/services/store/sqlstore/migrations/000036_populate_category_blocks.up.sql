16e8ac49c4dcbb956efa7eb1bce7922e9b6d1b2d
Harshil Sharma <harshilsharma63@gmail.com>
2022-12-07 17:16:34 +0530

    moved existing migrations fto create space for expanded moigration

---
+++ server/services/store/sqlstore/migrations/000036_populate_category_blocks.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS {{.prefix}}category_boards (
+    id varchar(36) NOT NULL,
+    user_id varchar(36) NOT NULL,
+    category_id varchar(36) NOT NULL,
+    board_id VARCHAR(36) NOT NULL,
+    create_at BIGINT,
+    update_at BIGINT,
+    delete_at BIGINT,
+    PRIMARY KEY (id)
+) {{if .mysql}}DEFAULT CHARACTER SET utf8mb4{{end}};
+
+CREATE INDEX idx_categoryboards_category_id ON {{.prefix}}category_boards(category_id);
