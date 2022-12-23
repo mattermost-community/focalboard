7af529a44f41d54810a18c33bbfe2a7a1847a1c1
Jesús Espino <jespinog@gmail.com>
2022-12-23 10:17:48 +0100

    Putting pages behind a feature flag

--- mattermost-plugin/webapp/src/index.tsx
+++ mattermost-plugin/webapp/src/index.tsx
@@ -167,6 +167,7 @@ export default class Plugin {
             messages: getMessages(getCurrentLanguage())
         }, cache)
 
+        const clientConfig = await octoClient.getClientConfig()
 
         this.registry = registry
 
@@ -321,16 +322,18 @@ export default class Plugin {
                 true,
             )
 
-            this.registry.registerProduct(
-                '/pages',
-                'file-text-outline',
-                'Pages',
-                '/pages',
-                (props) => <MainApp {...props} baseURL={subpath + '/pages'}/>,
-                () => <HeaderComponent/>,
-                () => null,
-                true,
-            )
+            if (clientConfig?.featureFlags?.pagesProduct) {
+                this.registry.registerProduct(
+                    '/pages',
+                    'file-text-outline',
+                    'Pages',
+                    '/pages',
+                    (props) => <MainApp {...props} baseURL={subpath + '/pages'}/>,
+                    () => <HeaderComponent/>,
+                    () => null,
+                    true,
+                )
+            }
 
             const goToFocalboardTemplate = () => {
                 const currentTeam = mmStore.getState().entities.teams.currentTeamId
@@ -349,25 +352,28 @@ export default class Plugin {
                     mmStore.dispatch(toggleRHSPlugin)
                 }, intl.formatMessage({id: 'AppBar.Tooltip', defaultMessage: 'Toggle Linked Boards'}))
                 // this.registry.registerAppBarComponent(,  , intl.formatMessage({id: 'AppBar.TooltipPages', defaultMessage: 'Toggle Linked Pages'}))
-                // HACK: Register second app bar component
-                // TODO: Remove this hack whenever is possible
-                const data = {
-                    id: Utils.createGuid(IDType.None),
-                    pluginId: manifest.id+'pages',
-                    iconUrl: Utils.buildURL(appBarIconPages, true),
-                    action: () => {
-                        windowAny.frontendBaseURL = subpath + '/pages'
-                        mmStore.dispatch(toggleRHSPagesPlugin)
-                    },
-                    tooltipText: intl.formatMessage({id: 'AppBar.TooltipPages', defaultMessage: 'Toggle Linked Pages'}),
-                }
 
-                mmStore.dispatch({
-                    type: 'RECEIVED_PLUGIN_COMPONENT',
-                    name: 'AppBar',
-                    data,
-                } as any);
-                this.pagesAppIconId = data.id
+                if (clientConfig?.featureFlags?.pagesProduct) {
+                    // HACK: Register second app bar component
+                    // TODO: Remove this hack whenever is possible
+                    const data = {
+                        id: Utils.createGuid(IDType.None),
+                        pluginId: manifest.id+'pages',
+                        iconUrl: Utils.buildURL(appBarIconPages, true),
+                        action: () => {
+                            windowAny.frontendBaseURL = subpath + '/pages'
+                            mmStore.dispatch(toggleRHSPagesPlugin)
+                        },
+                        tooltipText: intl.formatMessage({id: 'AppBar.TooltipPages', defaultMessage: 'Toggle Linked Pages'}),
+                    }
+
+                    mmStore.dispatch({
+                        type: 'RECEIVED_PLUGIN_COMPONENT',
+                        name: 'AppBar',
+                        data,
+                    } as any);
+                    this.pagesAppIconId = data.id
+                }
             }
 
             this.registry.registerPostWillRenderEmbedComponent(
@@ -431,8 +437,7 @@ export default class Plugin {
             </ReduxProvider>
         ))
 
-        const config = await octoClient.getClientConfig()
-        if (config?.telemetry) {
+        if (clientConfig?.telemetry) {
             let rudderKey = TELEMETRY_RUDDER_KEY
             let rudderUrl = TELEMETRY_RUDDER_DATAPLANE_URL
 
@@ -451,7 +456,7 @@ export default class Plugin {
                 }
                 rudderAnalytics.load(rudderKey, rudderUrl, rudderCfg)
 
-                rudderAnalytics.identify(config?.telemetryid, {}, TELEMETRY_OPTIONS)
+                rudderAnalytics.identify(clientConfig?.telemetryid, {}, TELEMETRY_OPTIONS)
 
                 rudderAnalytics.page('BoardsLoaded', '',
                     TELEMETRY_OPTIONS.page,
