7ac6f2541cd24c5a68ee84b89d84b5c28f88c112
Harshil Sharma <harshilsharma63@gmail.com>
2022-12-28 16:55:00 +0530

    Unique constraint migration

---
+++ server/services/store/sqlstore/migrations/000038_hidden_boards_from_preferences.up.sql
@@ -0,0 +1,19 @@
+{{if .mysql}}
+    UPDATE {{.prefix}}category_boards AS fcb 
+        JOIN Preferences p 
+        ON fcb.user_id = p.userid
+        AND p.category = 'focalboard'
+        AND p.name = 'hiddenBoardIDs'
+        SET hidden = true
+        WHERE p.value LIKE concat('%', fcb.board_id, '%');
+{{end}}
+
+{{if .postgres}}
+    UPDATE {{.prefix}}category_boards fcb 
+    SET hidden = true
+    FROM preferences p
+    WHERE p.userid = fcb.user_id 
+    AND p.category = 'focalboard'
+    AND p.name = 'hiddenBoardIDs'
+    AND p.value like concat('%', fcb.board_id, '%');
+{{end}}
