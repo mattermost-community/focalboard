59d6251531e381bd49df18189e02f1a6fc511784
Harshil Sharma <harshilsharma63@gmail.com>
2022-12-29 14:37:33 +0530

    Added SQLITE support in few more migrations

--- server/services/store/sqlstore/migrations/000034_category_boards_remove_unused_delete_at_column.up.sql
+++ server/services/store/sqlstore/migrations/000034_category_boards_remove_unused_delete_at_column.up.sql
@@ -1,1 +1,20 @@
-{{ dropColumnIfNeeded "category_boards" "delete_at" }}
+{{ if or .postgres .mysql }}
+    {{ dropColumnIfNeeded "category_boards" "delete_at" }}
+{{end}}
+
+{{if .sqlite}}
+    ALTER TABLE {{.prefix}}category_boards RENAME TO {{.prefix}}category_boards_old;
+    CREATE TABLE {{.prefix}}category_boards (
+        id varchar(36) NOT NULL,
+        user_id varchar(36) NOT NULL,
+        category_id varchar(36) NOT NULL,
+        board_id VARCHAR(36) NOT NULL,
+        create_at BIGINT,
+        update_at BIGINT,
+        sort_order BIGINT,
+        PRIMARY KEY (id)
+    );
+
+    INSERT INTO {{.prefix}}category_boards SELECT id, user_id, category_id, board_id, create_at, update_at, sort_order from {{.prefix}}category_boards_old;
+    DROP TABLE {{.prefix}}category_boards_old;
+{{end}}
--- server/services/store/sqlstore/migrations/000035_category_boards_remove_unused_id_column.up.sql
+++ server/services/store/sqlstore/migrations/000035_category_boards_remove_unused_id_column.up.sql
@@ -1,1 +1,18 @@
-{{ dropColumnIfNeeded "category_boards" "id" }}
+{{if or .postgres .mysql}}
+    {{ dropColumnIfNeeded "category_boards" "id" }}
+{{end}}
+
+{{if .sqlite}}
+    ALTER TABLE {{.prefix}}category_boards RENAME TO {{.prefix}}category_boards_old;
+    CREATE TABLE {{.prefix}}category_boards (
+        user_id varchar(36) NOT NULL,
+        category_id varchar(36) NOT NULL,
+        board_id VARCHAR(36) NOT NULL,
+        create_at BIGINT,
+        update_at BIGINT,
+        sort_order BIGINT
+    );
+
+    INSERT INTO {{.prefix}}category_boards SELECT user_id, category_id, board_id, create_at, update_at, sort_order from {{.prefix}}category_boards_old;
+    DROP TABLE {{.prefix}}category_boards_old;
+{{end}}
