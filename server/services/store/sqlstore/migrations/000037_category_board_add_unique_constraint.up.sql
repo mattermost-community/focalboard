cc22b84c7542250c257403e3ae7032fa2bc892d3
Harshil Sharma <harshilsharma63@gmail.com>
2022-12-30 09:21:02 +0530

    Used old-fashioned way to add unique constraint

--- server/services/store/sqlstore/migrations/000037_category_board_add_unique_constraint.up.sql
+++ server/services/store/sqlstore/migrations/000037_category_board_add_unique_constraint.up.sql
@@ -3,5 +3,20 @@
 {{end}}
 
 {{if .sqlite}}
-    CREATE UNIQUE INDEX IF NOT EXISTS unique_user_category_board ON {{.prefix}}category_boards(user_id, board_id);
+    ALTER TABLE {{.prefix}}category_boards RENAME TO {{.prefix}}category_boards_old;
+
+    CREATE TABLE {{.prefix}}category_boards (
+        user_id varchar(36) NOT NULL,
+        category_id varchar(36) NOT NULL,
+        board_id VARCHAR(36) NOT NULL,
+        create_at BIGINT,
+        update_at BIGINT,
+        sort_order BIGINT,
+        hidden boolean,
+        CONSTRAINT unique_user_category_board UNIQUE (user_id, board_id)
+    );
+
+    INSERT INTO {{.prefix}}category_boards SELECT * FROM {{.prefix}}category_boards_old;
+    DROP TABLE {{.prefix}}category_boards_old;
+
 {{end}}
