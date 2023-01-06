938e7f1c82c7af00115248e284f302c28557f040
Scott Bishel <scott.bishel@mattermost.com>
2023-01-05 21:02:07 -0700

    Fix release-7.7 - revert #4372 (#4431)
    * Revert "Cherry-pick 4404: expose boards APIs (#4426)"
    
    This reverts commit 5c6ac204369bd85063f7184e67de3f619cd658ad.
    
    * Search boards by prop (#4291)
    
    * SearchBoardsForUser API with property name search.
    
    (cherry picked from commit b63542fb63d5245c8c306e45f1f97cd108f70c37)
    
    * update snapshots for date format
    
    (cherry picked from commit 7257308d2711ed1c92320fa5495f5676590812bf)
    
    Co-authored-by: Doug Lauder <wiggin77@warpmail.net>

--- server/app/permissions.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import (
-	mm_model "github.com/mattermost/mattermost-server/v6/model"
-)
-
-func (a *App) HasPermissionToBoard(userID, boardID string, permission *mm_model.Permission) bool {
-	return a.permissions.HasPermissionToBoard(userID, boardID, permission)
-}
