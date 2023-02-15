034d9db23c7d00c2532da8b498468288736d5c9d
Harshil Sharma <harshilsharma63@gmail.com>
2023-02-15 07:49:05 +0530

    Fixed server lint

--- server/api/api.go
+++ server/api/api.go
@@ -227,7 +227,7 @@ func jsonStringResponse(w http.ResponseWriter, code int, message string) { //nol
 	fmt.Fprint(w, message)
 }
 
-func jsonBytesResponse(w http.ResponseWriter, code int, json []byte) {
+func jsonBytesResponse(w http.ResponseWriter, code int, json []byte) { //nolint:unparam
 	setResponseHeader(w, "Content-Type", "application/json")
 	w.WriteHeader(code)
 	_, _ = w.Write(json)
