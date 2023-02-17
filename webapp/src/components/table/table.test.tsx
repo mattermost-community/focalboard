2e537785c47f9eaec0fb68ac25d11336b66c819b
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2023-02-17 13:05:37 -0500

    removing unused comments

--- webapp/src/components/table/table.test.tsx
+++ webapp/src/components/table/table.test.tsx
@@ -36,13 +36,8 @@ jest.mock('../../utils')
 jest.mock('../../telemetry/telemetryClient')
 const mockedMutator = mocked(Mutator, true)
 
-// const mockedUtils = mocked(Utils, true)
-
-// mockedUtils.createGuid = jest.requireActual('../../utils').Utils.createGuid
-
 describe('components/table/Table', () => {
     const board = TestBlockFactory.createBoard()
-
     const view = TestBlockFactory.createBoardView(board)
     view.fields.viewType = 'table'
     view.fields.groupById = undefined
