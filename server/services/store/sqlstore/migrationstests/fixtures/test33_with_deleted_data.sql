263492415037606da1c59250306a50b487f1517d
Harshil Sharma <18575143+harshilsharma63@users.noreply.github.com>
2023-03-30 14:20:50 +0530

    Cherrypicking 22606 to old focalboard (#4673)

--- server/services/store/sqlstore/migrationstests/fixtures/test33_with_deleted_data.sql
+++ server/services/store/sqlstore/migrationstests/fixtures/test33_with_deleted_data.sql
@@ -1,4 +1,6 @@
-INSERT INTO focalboard_category_boards values
+INSERT INTO focalboard_category_boards
+(id, user_id, category_id, board_id, create_at, update_at, delete_at, sort_order)
+values
 ('id-1', 'user_id-1', 'category-id-1', 'board-id-1', 1672988834402, 1672988834402, 0, 0),
 ('id-2', 'user_id-1', 'category-id-2', 'board-id-1', 1672988834402, 1672988834402, 0, 0),
 ('id-3', 'user_id-2', 'category-id-3', 'board-id-2', 1672988834402, 1672988834402, 1672988834402, 0),
