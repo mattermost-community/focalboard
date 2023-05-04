be8ada4a8a91b30c33db9066b961384d64709708
Scott Bishel <scott.bishel@mattermost.com>
2023-05-04 10:30:30 -0600

    update version to v7.9.4

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -37,7 +37,7 @@ class Constants {
     static readonly titleColumnId = '__title'
     static readonly badgesColumnId = '__badges'
 
-    static readonly versionString = '7.9.3'
+    static readonly versionString = '7.9.4'
     static readonly versionDisplayString = 'Mar 2023'
 
     static readonly archiveHelpPage = 'https://docs.mattermost.com/boards/migrate-to-boards.html'
