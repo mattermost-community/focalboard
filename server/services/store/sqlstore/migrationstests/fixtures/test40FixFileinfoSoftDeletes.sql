886f50e34202775504cc8284e472b5678603864b
Mattermost Build <build@mattermost.com>
2023-07-22 06:12:40 +0300

    Adds a database migration to restore the fileinfos that are deleted (#4815) (#4817)
    (cherry picked from commit 257cc5f1fd167cc791dd482a30b872da9743bff2)
    
    Co-authored-by: Miguel de la Cruz <miguel@mcrx.me>

---
+++ server/services/store/sqlstore/migrationstests/fixtures/test40FixFileinfoSoftDeletes.sql
@@ -0,0 +1,9 @@
+INSERT INTO FileInfo
+(Id, CreatorId, CreateAt, UpdateAt, DeleteAt)
+VALUES
+('fileinfo-1', 'user-id', 1, 1, 1000),
+('fileinfo-2', 'user-id', 1, 1, 1000),
+('fileinfo-3', 'user-id', 1, 1, 0),
+('fileinfo-4', 'boards', 1, 1, 2000),
+('fileinfo-5', 'boards', 1, 1, 2000),
+('fileinfo-6', 'boards', 1, 1, 0);
