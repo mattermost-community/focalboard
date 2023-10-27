60b2e2a3bef777c9b6559b2f4967ea7464d57373
BaoNguyen <bao.nguyenkim12@gmail.com>
2023-10-28 07:59:06 +0900

    Update win-wpf app to persist user settings (issue #314)

--- win-wpf/Focalboard/Properties/Settings.Designer.cs
+++ win-wpf/Focalboard/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Focalboard.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,18 @@ namespace Focalboard.Properties {
             }
             set {
                 this["WindowMaximized"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string WebAppSettingsBlob {
+            get {
+                return ((string)(this["WebAppSettingsBlob"]));
+            }
+            set {
+                this["WebAppSettingsBlob"] = value;
             }
         }
     }
