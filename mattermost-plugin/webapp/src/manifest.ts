1c82888fd04f4637c62f5004093da3174b9f573a
Rajat Dabade <a-rajat.dabade@mattermost.com>
2024-07-29 16:56:12 +0530

    refactor: removed mattermost-plugin folder

--- mattermost-plugin/webapp/src/manifest.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-import manifest from '../../plugin.json'
-
-export default manifest
-export const id = manifest.id
-export const version = manifest.version
